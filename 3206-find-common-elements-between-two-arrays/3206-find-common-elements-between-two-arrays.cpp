18c5b4e3ff7ec978a7f6d0202c9eb8929a40ecb8
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2025-11-22 21:51:19 +0530

    Time: 35 ms (5.55%), Space: 69.7 MB (33.39%) - LeetHub

--- 3206-find-common-elements-between-two-arrays/3206-find-common-elements-between-two-arrays.cpp
+++ 3206-find-common-elements-between-two-arrays/3206-find-common-elements-between-two-arrays.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     vector<int> findIntersectionValues(vector<int>& nums1, vector<int>& nums2) {
-        vector<int>freq1(101 , 0);
-        vector<int>freq2(101 , 0);
+        unordered_map<int , int> mp1;
+        unordered_map<int , int> mp2;
         for(int i = 0 ; i < nums1.size() ; i++){
-            freq1[nums1[i]]++;
+            mp1[nums1[i]]++;
         }
         for(int i = 0 ; i < nums2.size() ; i++){
-            freq2[nums2[i]]++;
+            mp2[nums2[i]]++;
         }
         int count1 = 0 ;
-        for(int i = 0 ; i < nums2.size() ; i++){
-            if(freq1[nums2[i]] > 0){
-                count1++;
-            }
-        }
         int count2 = 0 ;
+        for(int i = 0 ; i < nums2.size() ; i++){
+            if(mp1.find(nums2[i]) != mp1.end()) count1++ ;
+        }
         for(int i = 0 ; i < nums1.size() ; i++){
-            if(freq2[nums1[i]] > 0){
-                count2++;
-            }
+            if(mp2.find(nums1[i]) != mp2.end()) count2++;
         }
 
         return {count2 , count1};
