9834955a47c2ab272ca7dc6870ca9227848adf7b
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2025-11-22 21:44:57 +0530

    Time: 3 ms (85.29%), Space: 66.4 MB (58.59%) - LeetHub

--- 3206-find-common-elements-between-two-arrays/3206-find-common-elements-between-two-arrays.cpp
+++ 3206-find-common-elements-between-two-arrays/3206-find-common-elements-between-two-arrays.cpp
@@ -1,17 +1,23 @@
 class Solution {
 public:
     vector<int> findIntersectionValues(vector<int>& nums1, vector<int>& nums2) {
-        set<int> s1 (nums1.begin() , nums1.end());
-        set<int> s2 (nums2.begin() , nums2.end());
+        vector<int>freq1(101 , 0);
+        vector<int>freq2(101 , 0);
+        for(int i = 0 ; i < nums1.size() ; i++){
+            freq1[nums1[i]]++;
+        }
+        for(int i = 0 ; i < nums2.size() ; i++){
+            freq2[nums2[i]]++;
+        }
         int count1 = 0 ;
-        for(int num : nums2){
-            if(s1.count(num)){
+        for(int i = 0 ; i < nums2.size() ; i++){
+            if(freq1[nums2[i]] > 0){
                 count1++;
             }
         }
         int count2 = 0 ;
-        for(int num : nums1){
-            if(s2.count(num)){
+        for(int i = 0 ; i < nums1.size() ; i++){
+            if(freq2[nums1[i]] > 0){
                 count2++;
             }
         }
