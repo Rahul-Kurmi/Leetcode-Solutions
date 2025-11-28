4faed61feeb05cdfa935f68a4b92e495c794a00d
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2025-11-28 22:48:04 +0530

    Time: 0 ms (100%), Space: 17.6 MB (52.06%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,39 +1,21 @@
 class Solution {
 public:
-    int binarySearch(vector<int>& arr , int target){
-        int low = 0 ;
-        int high = arr.size() - 1;
-        int mid = low + (high - low)/2 ;
-        while(low <= high){
-            if(arr[mid] == target){
-                return mid;
-            }
-            else if(target > arr[mid]){
-                low = mid + 1 ;
-            }
-            else{
-                high = mid - 1 ;
-            }
-            mid = low + (high - low) / 2 ;
-        }
-        return -1; ;
-    }
     vector<int> searchRange(vector<int>& nums, int target) {
-        if(nums.size() == 0) return {-1 , -1} ;
-        int index = binarySearch(nums , target);
-        if(index == -1 ) return {-1 , -1} ; 
-        int i = index ;
-        int j = index ;
-        int low , high ;
-        while(i >= 0 && nums[i] == nums[index]){
-                low = i ;
-                i--;
-        }
-        while(j < nums.size() && nums[j] == nums[index]){
-                high = j ;
-                j++;
+        auto left = lower_bound(nums.begin(), nums.end(), target);
+        auto right = upper_bound(nums.begin(), nums.end(), target);
+
+        int firstPos = -1, lastPos = -1;
+
+        // must check that left != end before dereferencing
+        if (left != nums.end() && *left == target) {
+            firstPos = left - nums.begin();
         }
 
-        return {low , high} ;
+        // safe check for last position
+        if (right != nums.begin() && *(right - 1) == target) {
+            lastPos = (right - nums.begin()) - 1;
+        }
+
+        return {firstPos, lastPos};
     }
 };
