0acc1ee494b130cb7a1ac2845861e1a4e8fda368
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2025-12-04 01:05:56 +0530

    Time: 0 ms (100%), Space: 35.9 MB (66.38%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,17 +1,42 @@
 class Solution {
 public:
-    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        int low = 0 ;
-        int high = arr.size() - 1 ;
 
-        while(high - low >= k){
-            if(abs(arr[high] - x) >= abs(arr[low] - x)){
-                high--;
-            }else{
-                low++;
+    int lowerbound(vector<int>& arr, int x) {
+        int start = 0;
+        int end = arr.size() - 1;
+        int ans = end;
+
+        while (start <= end) {
+            int mid = (start + end) / 2;
+
+            if (arr[mid] >= x) {
+                ans = mid; 
+                end = mid - 1; 
+            } else {
+                start = mid + 1; 
             }
         }
+        return ans; 
+    }
 
-        return vector<int>(arr.begin() + low , arr.begin() + high + 1 ) ;
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int high = lowerbound(arr , x);
+        int low = high - 1;
+        while(k--){
+            if(low < 0){
+                high++;
+            }
+            else if(high >= arr.size()){
+                low--;
+            }
+            else if(arr[high] - x >= x - arr[low]){
+                low--;
+            }
+            else{
+                high++;
+            }
+        }  
+
+        return vector<int>(arr.begin() + (low + 1) , arr.begin() + high ) ;
     }
 };
