b686d9d5dff2762374cead41c1ebe0370df85670
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2025-12-15 17:44:50 +0530

    Time: 16 ms (6.07%), Space: 16.2 MB (5.31%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,64 +1,19 @@
 class Solution {
 public:
     string intToRoman(int num) {
+        map<int, string> romanMap = {
+            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+            {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
+            {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
+        };
         string ans = "";
-
-        while (num > 0) {
-
-            if (num >= 1000) {
-                ans += "M";
-                num -= 1000;
-            }
-            else if (num >= 900) {
-                ans += "CM";
-                num -= 900;
-            }
-            else if (num >= 500) {
-                ans += "D";
-                num -= 500;
-            }
-            else if (num >= 400) {
-                ans += "CD";
-                num -= 400;
-            }
-            else if (num >= 100) {
-                ans += "C";
-                num -= 100;
-            }
-            else if (num >= 90) {
-                ans += "XC";
-                num -= 90;
-            }
-            else if (num >= 50) {
-                ans += "L";
-                num -= 50;
-            }
-            else if (num >= 40) {
-                ans += "XL";
-                num -= 40;
-            }
-            else if (num >= 10) {
-                ans += "X";
-                num -= 10;
-            }
-            else if (num >= 9) {
-                ans += "IX";
-                num -= 9;
-            }
-            else if (num >= 5) {
-                ans += "V";
-                num -= 5;
-            }
-            else if (num >= 4) {
-                ans += "IV";
-                num -= 4;
-            }
-            else {
-                ans += "I";
-                num -= 1;
+        for(auto it = romanMap.rbegin() ; it != romanMap.rend() ; it++){
+            int ele = it->first ;
+            while(num !=0 && num >= ele){
+                ans += it->second ;
+                num = num - ele ;
             }
         }
-
-        return ans;
+        return ans ;
     }
 };
