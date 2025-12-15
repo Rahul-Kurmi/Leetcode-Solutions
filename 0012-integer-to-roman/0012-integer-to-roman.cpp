ae432345f1255fff68837f9544737018d5a50300
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2025-12-15 17:45:54 +0530

    Time: 5 ms (37.25%), Space: 12.7 MB (32.32%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     string intToRoman(int num) {
-        map<int, string> romanMap = {
+        vector<pair<int, string>> romanPairs = {
             {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
             {100, "C"}, {90, "XC"}, {50, "L"}, {40, "XL"},
             {10, "X"}, {9, "IX"}, {5, "V"}, {4, "IV"}, {1, "I"}
         };
+        
         string ans = "";
-        for(auto it = romanMap.rbegin() ; it != romanMap.rend() ; it++){
-            int ele = it->first ;
-            while(num !=0 && num >= ele){
-                ans += it->second ;
-                num = num - ele ;
+        for(auto it : romanPairs) {
+            int ele = it.first;
+            while(num != 0 && num >= ele) {
+                ans += it.second;
+                num = num - ele;
             }
         }
-        return ans ;
+        return ans;
     }
 };
