a8620970a8db405fe5a25ecfa512ec83a3d0f132
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2025-12-31 16:24:48 +0530

    Time: 4 ms (51.96%), Space: 17.1 MB (25.58%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,24 +1,23 @@
-bool cmp(string a , string b){
-    return a+b > b+a;
-}
-
 class Solution {
 public:
     string largestNumber(vector<int>& nums) {
-        vector<string> store ;
-        for(auto it : nums){
+        vector<string> store;
+        for (auto it : nums) {
             store.push_back(to_string(it));
         }
 
-        sort(store.begin() , store.end() , cmp);
+        sort(store.begin(), store.end(), [](string a , string b){
+            return a+b > b+a ;
+        });
 
-        if(store[0] == "0") return "0";
+        // 🔹 handle all zeros case
+        if (store[0] == "0") return "0";
 
         string ans = "";
-        for(auto i = store.begin() ; i != store.end() ; i++){
-            ans = ans + *i ;
+        for (auto &s : store) {
+            ans += s;
         }
 
-        return ans ;
+        return ans;
     }
 };
