2172d553e0ac04274aea34bdf6c327c8a9d6f4fb
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-01-09 19:34:59 +0530

    Time: 2 ms (37.41%), Space: 9.1 MB (57.68%) - LeetHub

--- 0415-add-strings/0415-add-strings.cpp
+++ 0415-add-strings/0415-add-strings.cpp
@@ -3,35 +3,17 @@ public:
     string addStrings(string num1, string num2) {
         int i = num1.size() - 1 ;
         int j = num2.size() - 1 ;
-        int carry = 0 ;
+        int carry =  0 ;
         string ans = "";
-        while(i >= 0 && j >= 0){
-            int sum = (num1[i] - '0') + (num2[j] - '0') + carry ;
-            int store = sum % 10 ;
-            carry = sum / 10 ;
-            ans.push_back(store + '0');
-            i--;
-            j--;
-        }
 
-        while(i >= 0){
-            int sum = (num1[i] - '0') + carry;
-            int store = sum % 10 ;
-            carry = sum / 10 ;
-            ans.push_back(store + '0');
-            i--;
-        }
+        while(i >= 0 || j >= 0 || carry){
+            int sum = carry ;
 
-        while(j >= 0){
-            int sum = (num2[j] - '0') + carry;
-            int store = sum % 10 ;
-            carry = sum / 10 ;
-            ans.push_back(store + '0');
-            j--;
-        }
+            if(i >= 0) sum += num1[i--] - '0';
+            if(j >= 0) sum += num2[j--] - '0';
 
-        if(carry != 0){
-            ans.push_back(carry + '0');
+            ans.push_back((sum % 10) + '0');
+            carry = sum / 10 ;
         }
 
         reverse(ans.begin() , ans.end());
