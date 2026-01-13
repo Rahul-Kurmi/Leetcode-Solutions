8df1d0a16f5feaa14558cf7bfe07decfcd3d6798
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-01-13 22:44:22 +0530

    Time: 0 ms (100%), Space: 11.3 MB (43.2%) - LeetHub

--- 0273-integer-to-english-words/0273-integer-to-english-words.cpp
+++ 0273-integer-to-english-words/0273-integer-to-english-words.cpp
@@ -1,60 +1,63 @@
 class Solution {
 public:
-     map<int, string> mp {
-            {1000000000, "Billion"},
-            {1000000, "Million"},
-            {1000, "Thousand"},
-            {100, "Hundred"},
-            {90, "Ninety"},
-            {80, "Eighty"},
-            {70, "Seventy"},
-            {60, "Sixty"},
-            {50, "Fifty"},
-            {40, "Forty"},
-            {30, "Thirty"},
-            {20, "Twenty"},
-            {19, "Nineteen"},
-            {18, "Eighteen"},
-            {17, "Seventeen"},
-            {16, "Sixteen"},
-            {15, "Fifteen"},
-            {14, "Fourteen"},
-            {13, "Thirteen"},
-            {12, "Twelve"},
-            {11, "Eleven"},
-            {10, "Ten"},
-            {9, "Nine"},
-            {8, "Eight"},
-            {7, "Seven"},
-            {6, "Six"},
-            {5, "Five"},
-            {4, "Four"},
-            {3, "Three"},
-            {2, "Two"},
-            {1, "One"}
-        };
+    vector<string> below20 = {
+        "", "One", "Two", "Three", "Four", "Five", "Six",
+        "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve",
+        "Thirteen", "Fourteen", "Fifteen", "Sixteen",
+        "Seventeen", "Eighteen", "Nineteen"
+    };
+
+    vector<string> tens = {
+        "", "", "Twenty", "Thirty", "Forty",
+        "Fifty", "Sixty", "Seventy", "Eighty", "Ninety"
+    };
+
+    string convertBelow1000(int num){
+        string ans = "";
+        if(num >= 100){
+            ans += below20[num/100] + " Hundred";
+            num = num % 100;
+            if(num > 0) ans += " ";
+        }
+
+        if(num >= 20){
+            ans += tens[num/10];
+            num = num % 10 ;
+            if(num > 0) ans += " " + below20[num];
+        }
+        else if(num > 0){
+            ans += below20[num];
+        }
+
+        return ans ;
+    }
 
     string numberToWords(int num) {
+        if(num == 0 ) return "Zero";
         string ans = "";
-        if(num == 0){
-            return "Zero";
+
+        if(num >= 1000000000){
+            ans += convertBelow1000(num/1000000000) + " Billion";
+            num = num % 1000000000;
+            if(num > 0 ) ans += " ";
         }
 
-        for(auto it =  mp.rbegin() ; it != mp.rend() ; it++){
-            if(num >= it->first){
-                string a = ""; 
-                if(num >= 100){
-                    a =  numberToWords(num/it->first) + " ";
-                }
-                string b = it->second;
-                string c = "";
-                if(num % it->first != 0){ 
-                    c = " " + numberToWords(num % it->first);
-                }
-                return a + b + c ;
-            }
+        if(num >= 1000000){
+            ans += convertBelow1000(num/1000000) + " Million";
+            num = num % 1000000;
+            if(num > 0 ) ans += " ";
         }
 
-        return "";
+        if(num >= 1000){
+            ans += convertBelow1000(num/1000) + " Thousand";
+            num = num % 1000;
+            if(num > 0) ans += " ";
+        }
+
+        if(num > 0){
+            ans += convertBelow1000(num);
+        }
+
+        return ans ;
     }
 };
