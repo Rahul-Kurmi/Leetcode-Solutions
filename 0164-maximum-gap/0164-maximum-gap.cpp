c7da71791f4d08ae7ff9df35f8104e6ad578c510
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-01-19 22:04:48 +0530

    Time: 47 ms (35.31%), Space: 85.7 MB (89.27%) - LeetHub

---
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maximumGap(vector<int>& arr) {
+        sort(arr.begin() , arr.end());
+        if(arr.size() <= 1) return 0 ;
+        int i = 1 ;
+        int maxGap = INT_MIN;
+        while(i < arr.size()){
+            int diff = arr[i] - arr[i-1];
+            maxGap = max(diff, maxGap);
+            i++;
+        }
+        return maxGap ;
+    }
+};
