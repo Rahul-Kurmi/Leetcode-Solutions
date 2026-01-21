68c859be30532b486878b6f662ce1f41c97f1761
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-01-21 23:18:52 +0530

    Time: 0 ms (100%), Space: 7.8 MB (48.06%) - LeetHub

---
+++ 0342-power-of-four/0342-power-of-four.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isValidDigit(int n , int i , int val){
+        if(val == n) return true;
+        if(val > n) return false ;
+        if(val > INT_MAX/i) return false;
+        return isValidDigit(n , i , val*i);
+    }
+
+    bool isPowerOfFour(int n) {
+        return isValidDigit(n , 4, 1);
+    }
+};
