231c1301f89fc5be27327ed1ad60f4e926989598
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-02-03 21:49:59 +0530

    Time: 136 ms (58.05%), Space: 71 MB (83.44%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,39 +1,35 @@
 class Solution {
 public:
-
-    void merge(vector<int>&arr, vector<int>&temp , int start , int end){
-        int mid = start + (end - start)/2 ;
-
-        int i = start ;
-        int j = mid+1;
-        int k = start ;
-        while(i <= mid && j <= end){
-            if(arr[i] < arr[j]) temp[k++] = arr[i++];
-            else temp[k++] = arr[j++];
-        }
-
-        while(i <= mid) temp[k++] = arr[i++];
-        while(j <= end) temp[k++] = arr[j++];
-
-        for(int i = start ; i <= end ; i++){
-            arr[i] = temp[i];
+    void merge(vector<int>&arr, int start , int end){
+        int size = end - start + 1 ;
+        int gap = size / 2 + size % 2 ;
+        while(gap > 0){
+            int i = start ;
+            int j = start + gap ;
+            while(j <= end){
+                if(arr[i] > arr[j]){
+                    swap(arr[i] , arr[j]);
+                }
+                i++;
+                j++;
+            }
+            gap = (gap == 1) ? 0 : (gap / 2) + (gap % 2) ;
         } 
     }
 
-    void mergeSort(vector<int>&arr , vector<int>& temp , int start , int end){
+    void mergeSort(vector<int>&arr, int start , int end){
         if(start >= end) return ;
         
         int mid = start + (end - start) / 2 ;
 
-        mergeSort(arr, temp, start, mid);
-        mergeSort(arr, temp, mid+1, end);
-        merge(arr, temp, start, end);
+        mergeSort(arr,  start, mid);
+        mergeSort(arr, mid+1, end);
+        merge(arr, start, end);
 
     }
 
     vector<int> sortArray(vector<int>& nums) {
-        vector<int> temp(nums.size());
-        mergeSort(nums , temp , 0 , nums.size() - 1);
+        mergeSort(nums , 0 , nums.size() - 1);
         return nums ;
     }
 };
