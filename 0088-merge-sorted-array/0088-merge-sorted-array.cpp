5b67493fe31dc1b16901753fec19ac731e252813
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-02-04 23:24:12 +0530

    Time: 0 ms (100%), Space: 12.4 MB (9.08%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,37 +1,24 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int total_elements = m + n ;
-        int gap = (total_elements / 2 ) + (total_elements % 2) ;
-        while(gap > 0){
-            int i = 0 ;
-            int j = i + gap ;
-            while(j < total_elements){
-                if(i < m && j < m){
-                    if(nums1[i] > nums1[j]){
-                        swap(nums1[i] , nums1[j]);
-                    }
-                }
-                else if(i < m && j >= m){
-                    if(nums1[i] > nums2[j-m]){
-                        swap(nums1[i] , nums2[j-m]);
-                    }
-                }
-                else if(i >= m && j >= m){
-                    if(nums2[i - m] > nums2[j - m]){
-                        swap(nums2[i - m] , nums2[j - m]);
-                    }
-                }
-                i++;
-                j++;
-            }
-            gap = (gap == 1) ? 0 : (gap / 2 ) + (gap % 2) ;
-        } 
-
-
         int k = m ;
         for(int i = 0 ; i < n ; i++){
             nums1[k++] = nums2[i];
         }
+
+        int gap = (nums1.size() / 2 ) + (nums1.size() % 2) ;
+        while(gap > 0){
+            int i = 0 ;
+            int j = i + gap ;
+            while(j < nums1.size()){
+                if(nums1[i] > nums1[j]){
+                    swap(nums1[i] , nums1[j]);
+                }
+                i++;
+                j++;
+            }
+
+            gap = (gap == 1) ? 0 : (gap / 2) + (gap % 2);
+        } 
     }
 };
