f113f0363a7950915506fcbecd914c353b9e7a52
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-02-05 19:44:01 +0530

    Time: 18 ms (5.32%), Space: 71.8 MB (51.41%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,17 +1,47 @@
 class Solution {
 public:
-    int maxSubArray(vector<int>& nums) {
-        int maxSum = INT_MIN ;
-        int sum = 0 ;
-        for(int i = 0 ; i < nums.size() ; i++){
-            sum = sum + nums[i];
-            if(sum > maxSum){
-                maxSum = sum;
-            }
-            if(sum < 0){
-                sum = 0 ;
-            }
+    void findCrossSum(vector<int>& arr, int start, int end, int& maxSum){
+        int mid = start + (end - start) / 2 ;
+
+        int maxLeftBorderSum = INT_MIN ;
+        int maxRightBorderSum = INT_MIN ;
+
+        int leftBorderSum = 0 ;
+        int rightBorderSum = 0 ;
+
+        for(int i = mid ; i >= start ; i--){
+            leftBorderSum += arr[i];
+            maxLeftBorderSum = max(maxLeftBorderSum , leftBorderSum);
         }
-        return maxSum ;
+
+        for(int i = mid+1; i <= end ; i++){
+            rightBorderSum += arr[i];
+            maxRightBorderSum = max(maxRightBorderSum , rightBorderSum);
+        }
+
+        int crossSum = maxLeftBorderSum + maxRightBorderSum ;
+
+        maxSum = max(maxSum , crossSum);
+
+    }
+
+    void maxSubArrayRec(vector<int>& arr, int start, int end, int& maxSum){
+        if(start == end){
+            maxSum = max(maxSum , arr[start]);
+            return ;
+        }
+
+        int mid = start + (end - start) / 2 ;
+
+        maxSubArrayRec(arr, start , mid , maxSum);
+        maxSubArrayRec(arr, mid+1 , end , maxSum);
+
+        findCrossSum(arr, start , end , maxSum);
+    }
+
+    int maxSubArray(vector<int>& nums) {
+        int ans = INT_MIN;
+        maxSubArrayRec(nums, 0, nums.size() - 1, ans);
+        return ans;
     }
 };
