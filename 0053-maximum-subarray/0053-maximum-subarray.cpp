b7fb9fad8869a72812c285bd79f370560c42c2fd
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-02-05 19:43:51 +0530

    Time: 0 ms (100%), Space: 71.8 MB (15.42%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,37 +1,17 @@
 class Solution {
 public:
-    int maxSumArrayHelper(vector<int>& arr, int start , int end){
-        if(start == end) {
-            return arr[start];
-        }
-
-        int mid = start + (end - start) / 2; 
- 
-        int leftSum = maxSumArrayHelper(arr, start , mid) ;
-        int rightSum = maxSumArrayHelper(arr, mid+1 , end) ;
-
-        int maxLeftBorderSum = INT_MIN ;
-        int maxRightBorderSum = INT_MIN ;
-
-        int leftBorderSum = 0 ;
-        int rightBorderSum = 0 ;
-
-        for(int i = mid ; i >= start ; i--){
-            leftBorderSum += arr[i];
-            maxLeftBorderSum = max(maxLeftBorderSum , leftBorderSum);
-        }
-
-        for(int i = mid + 1 ; i <= end ; i++){
-            rightBorderSum += arr[i];
-            maxRightBorderSum = max(maxRightBorderSum , rightBorderSum);
-        }
-
-        int crossSum = maxLeftBorderSum + maxRightBorderSum  ;
-
-        return max({leftSum , rightSum , crossSum});
-    }
-
     int maxSubArray(vector<int>& nums) {
-        return maxSumArrayHelper(nums, 0 , nums.size() - 1);
+        int maxSum = INT_MIN ;
+        int sum = 0 ;
+        for(int i = 0 ; i < nums.size() ; i++){
+            sum = sum + nums[i];
+            if(sum > maxSum){
+                maxSum = sum;
+            }
+            if(sum < 0){
+                sum = 0 ;
+            }
+        }
+        return maxSum ;
     }
 };
