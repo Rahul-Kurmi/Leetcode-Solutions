92bd47c5bcca45b47c1e2be4804ded87d0d6be41
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-02-06 12:51:03 +0530

    Time: 2 ms (59.29%), Space: 14.4 MB (40.69%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
+
     vector<vector<int>> ans;
 
-    void findingCombinationSum(vector<int>& arr, int target , int sum , vector<int>& store, int i){
+    void findingCombinationSum(vector<int>& arr, int target , int sum , vector<int>& store, int start){
         if(sum == target){
             ans.push_back(store);
             return ;
         }
-        if(sum > target || i >= arr.size()) return ;
+        if(sum > target || start >= arr.size()) return ;
 
-        // Include Case
-        store.push_back(arr[i]);
-        findingCombinationSum(arr, target , sum+arr[i], store, i);
-        store.pop_back();
-
-        // Exclude Case 
-        findingCombinationSum(arr, target , sum , store , i+1);
+        for(int i = start ; i < arr.size() ; i++){
+            store.push_back(arr[i]);
+            findingCombinationSum(arr, target - arr[i] , sum , store , i);
+            store.pop_back();
+        }
+        
     }
 
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
