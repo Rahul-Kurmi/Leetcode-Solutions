c5bf02e4e234a1421db8c6c3ca3afee64aabb508
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-02-15 19:26:33 +0530

    Time: 0 ms (100%), Space: 8.9 MB (59.48%) - LeetHub

--- 0067-add-binary/0067-add-binary.cpp
+++ 0067-add-binary/0067-add-binary.cpp
@@ -1,43 +1,31 @@
 class Solution {
 public:
     string addBinary(string a, string b) {
-        int i = a.size() - 1 ;
-        int j = b.size() - 1 ;
-        int carry = 0 ;
-
-        string ans = "" ;
-
-        while(i >= 0 && j >= 0){
-            int sum = (a[i] - '0') + (b[j] - '0') + carry ;
+        int i = a.size() - 1;
+        int j = b.size() - 1;
+        int carry = 0;  // ✅ Use int instead of char
+        string ans = "";
+        
+        // Process both strings from right to left
+        while(i >= 0 || j >= 0 || carry) {
+            int sum = carry;
             
-            char to_store = (sum % 2) + '0';
-            ans += to_store ;
-            carry = sum / 2 ;
-            i--;
-            j--;
+            if(i >= 0) {
+                sum += a[i] - '0';  // Convert char to int
+                i--;
+            }
+            
+            if(j >= 0) {
+                sum += b[j] - '0';  // Convert char to int
+                j--;
+            }
+            
+            ans += (sum % 2) + '0';  // Append current bit
+            carry = sum / 2;          // Calculate carry
         }
-
-        while(i >= 0){
-            int sum = (a[i] - '0') + carry ;
-            char to_store = (sum % 2) + '0';
-            ans += to_store ;
-            carry = sum / 2 ;
-            i--;
-        }
-
-        while(j >= 0){
-            int sum = (b[j] - '0') + carry ;
-            char to_store= (sum % 2) + '0' ;
-            ans += to_store ;
-            carry = sum / 2; 
-            j--;
-        }
-
-        if(carry != 0){
-            ans += carry + '0'; 
-        }
-
-        reverse(ans.begin() , ans.end());
-        return ans ;
+        
+        // Reverse the result
+        reverse(ans.begin(), ans.end());
+        return ans;
     }
 };
