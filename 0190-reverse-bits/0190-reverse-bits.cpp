69f0e16bd18ea429128d2911fe615c88a5934cb8
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-02-16 21:24:12 +0530

    Time: 0 ms (100%), Space: 8.2 MB (46.07%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -4,13 +4,11 @@ public:
         if(n == 0) return 0 ;
         int reverseAns = 0 ;
         for(int i = 31 ; i >= 0 ; i--){
-            reverseAns = reverseAns << 1 ;
-            if(n & 1){
-                reverseAns = reverseAns | 1 ; 
-            }else{
-                reverseAns = reverseAns | 0 ; 
-            }
-            n = n >> 1 ;
+            reverseAns <<= 1 ;
+
+            reverseAns = reverseAns | (n & 1) ;
+
+            n >>= 1;
         }
         return reverseAns;
     }
