af3f438a9e25b47d47008dccbc876d641f8d39e8
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-02-18 22:46:41 +0530

    Time: 839 ms (19.72%), Space: 11.8 MB (19.99%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -3,17 +3,24 @@ public:
     vector<vector<int>> directions = {{1, 0} , {-1,0} , {0, 1} , {0 , -1}};
     bool checkIfExist(vector<vector<char>>& board, string word, int row, int col, int iterate){
         if(iterate == word.size()) return true ;
+
+        if(row < 0 || col < 0 || row >= board.size() || col >= board[0].size() || board[row][col] == '$') {
+            return false ;
+        }
+
+        if(board[row][col] != word[iterate]) return false ;
+
         char temp = board[row][col];
         board[row][col] = '$' ;
 
         for(auto& dir : directions){
             int new_row = row + dir[0];
             int new_col = col + dir[1];
-            if(new_row >= 0 && new_row < board.size() && new_col >= 0 && new_col < board[0].size()){
-                if(board[new_row][new_col] == word[iterate] && checkIfExist(board , word , new_row , new_col , iterate+1)){
-                    return true ;
-                }
+
+            if(checkIfExist(board , word , new_row , new_col , iterate+1)){
+                return true ;
             }
+           
         }
 
         board[row][col] = temp ;
@@ -28,7 +35,7 @@ public:
         int start = word[0] ;
         for(int i = 0 ; i < row ; i++){
             for(int j = 0 ; j < col ; j++){
-                if(board[i][j] == start && checkIfExist(board, word , i , j , 1)){
+                if(board[i][j] == start && checkIfExist(board, word , i , j , 0)){
                     return true ;
                 }
             }
