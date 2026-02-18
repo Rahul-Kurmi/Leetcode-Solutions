f61349bd8707200342d8c56706cb974bd785c06b
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-02-18 22:45:31 +0530

    Time: 137 ms (96.08%), Space: 10.4 MB (83.67%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,11 +1,12 @@
 class Solution {
 public:
     
-    bool checkIfExist(vector<vector<char>>& board , string& word ,
-                      int row , int col, int wordCount){
+    void  checkIfExist(vector<vector<char>>& board , string& word ,
+                      int row , int col, int wordCount , bool& ans ){
         
         if(wordCount == word.size()){
-            return true ;
+            ans = true ;
+            return;
         }
 
         char temp = board[row][col];
@@ -16,44 +17,39 @@ public:
 
         // DOWN
         if(row+1 < rows && board[row+1][col] == word[wordCount]) {
-            if(checkIfExist(board , word, row+1 , col , wordCount+1))
-                return true;
+            checkIfExist(board , word, row+1 , col , wordCount+1 , ans );
         }
 
         // UP
         if(row-1 >= 0 && board[row-1][col] == word[wordCount]) {
-            if(checkIfExist(board , word, row-1 , col , wordCount+1))
-                return true;
+           checkIfExist(board , word, row-1 , col , wordCount+1, ans);
         }
 
         // LEFT
         if(col-1 >= 0 && board[row][col-1] == word[wordCount]) {
-            if(checkIfExist(board , word, row , col-1 , wordCount+1))
-                return true;
+            checkIfExist(board , word, row , col-1 , wordCount+1, ans);
         }
 
         // RIGHT
         if(col+1 < cols && board[row][col+1] == word[wordCount]) {
-            if(checkIfExist(board , word, row , col+1 , wordCount+1))
-                return true;
+            checkIfExist(board , word, row , col+1 , wordCount+1, ans);
         }
 
-        board[row][col] = temp;  // restore
-        return false ;
+        board[row][col] = temp;  // restore;
     }
 
     bool exist(vector<vector<char>>& board, string word) {
         int row = board.size();
         int col = board[0].size();
+        bool ans = false ;
 
         for(int i = 0 ; i < row ; i++){
             for(int j = 0 ; j < col ; j++){
-                if(board[i][j] == word[0] &&
-                   checkIfExist(board , word, i , j , 1)){
-                    return true ;
+                if(board[i][j] == word[0]){
+                    checkIfExist(board , word, i , j , 1, ans);
                 }
             }
         }
-        return false ;
+        return ans ;
     }
 };
