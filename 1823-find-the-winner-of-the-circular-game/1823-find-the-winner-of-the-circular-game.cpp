35c809e85e71b8f5f5f7ee0a06e1d06ca78a65dc
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-02-28 01:38:15 +0530

    Time: 3 ms (43.4%), Space: 8.6 MB (32.49%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.cpp
@@ -1,15 +1,42 @@
-class Solution {
-public:
-    int findWinnerIdx(int n , int k){
-        if(n == 1) return 0 ;
+    class Node {
+        public :
+            int data ;
+            Node * next ;
+            Node() : data(0) , next(nullptr) {};
+            Node(int data): data(data) , next(nullptr) {};
+    };
 
-        int idx = findWinnerIdx(n - 1 , k) ;
-        idx = (idx + k) % n ;
-        return idx ;
-    }
 
-    int findTheWinner(int n, int k) {
-        int result_idx = findWinnerIdx(n , k) + 1 ;
-        return result_idx ;
-    }
-};
+    class Solution {
+    public:
+        int findTheWinner(int n, int k) {
+        // Create circular linked list
+            Node* head = new Node(1);
+            Node* temp = head;
+
+            for(int i = 2; i <= n; i++){
+                temp->next = new Node(i);
+                temp = temp->next;
+            }
+
+            temp->next = head;   // make circular
+
+            Node* curr = head;
+            Node* prev = temp;   // last node initially
+
+            while(curr->next != curr){   // until one node remains
+
+                // move k-1 steps
+                for(int i = 1; i < k; i++){
+                    prev = curr;
+                    curr = curr->next;
+                }
+
+                // remove curr
+                prev->next = curr->next;
+                curr = curr->next;
+            }
+
+            return curr->data;
+        }
+    };
