a3a0164099cd5f835069f1bbdf37dcda730e45c0
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-03-01 01:28:06 +0530

    Time: 61 ms (34.41%), Space: 24.1 MB (25.1%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -9,19 +9,54 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        unordered_map<ListNode* , bool> mp ;
-        ListNode * temp = headA ;
-        while(temp != nullptr){
-            mp[temp] = true ;
-            temp = temp -> next ;
+        ListNode* a = headA ;
+        ListNode* b = headB ;
+        while(a -> next != nullptr && b -> next != nullptr){
+            if(a == b){
+                return a ;
+            }
+
+            a = a -> next ;
+            b = b -> next ;
         }
 
-        temp = headB;
-        while(temp != nullptr){
-            if(mp[temp] == true){
-                return temp;
+        if(a -> next == nullptr){
+            // b LL is bigger or equal 
+            // we have to find how much bigger 
+            int blen = 0 ;
+            while(b -> next != nullptr){
+                blen++;
+                b = b -> next ;
             }
-            temp = temp -> next ;
+
+            // Now Move headB blen times so that headA and haedB meet at same position
+            while(blen){
+                headB = headB -> next;
+                blen--;
+            }
+        }
+
+        // a LL is bigger
+        if(b -> next == nullptr){
+            int  alen = 0 ;
+            while(a -> next != nullptr){
+                alen++;
+                a = a -> next ;
+            }
+
+            //Now move headA alen times so that headA and headB can meet at same position
+            while(alen){
+                headA = headA -> next ;
+                alen--; 
+            }
+        }
+
+        while(headA != nullptr && headB != nullptr){
+            if(headA == headB){
+                return headA ;
+            }
+            headA = headA -> next ;
+            headB = headB -> next ;
         }
 
         return nullptr ;
