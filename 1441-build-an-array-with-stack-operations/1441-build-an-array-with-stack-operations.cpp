778cb0d5471e16dbe839e51a8172195537121193
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-03-01 21:28:27 +0530

    Time: 0 ms (100%), Space: 10.8 MB (54.34%) - LeetHub

---
+++ 1441-build-an-array-with-stack-operations/1441-build-an-array-with-stack-operations.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+        vector<string> ans ;
+        int idx = 0 ;
+        for(int i = 1 ; i <= n ; i++){
+            if(idx == target.size()) break;
+            if(target[idx] == i){
+                ans.push_back("Push");
+                idx++;
+            }else{
+                ans.push_back("Push");
+                ans.push_back("Pop");
+            }
+        }
+        return ans ;
+    }
+};
