60b23bdf2b7e27b44bba8b94a3ff5b1600ecd840
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-03-03 23:50:13 +0530

    Time: 70 ms (7.45%), Space: 273.8 MB (59.03%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.cpp
@@ -1,23 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* mergeNodes(ListNode* head) {
-        ListNode* dummy = new ListNode(-1);
-        ListNode* curr = dummy;
-        
-        ListNode* temp = head->next;  // skip first 0
-        int sum = 0;
-        
-        while(temp != nullptr){
-            if(temp->val == 0){
-                curr->next = new ListNode(sum);  // store sum
-                curr = curr->next;
-                sum = 0;                          // reset
-            } else {
-                sum += temp->val;                 // accumulate
+        if(!head) return nullptr ;
+        ListNode *slow = head , *fast = head -> next , *newLastNode = nullptr ;
+        int sum = 0 ;
+        while(fast){
+            if(fast -> val != 0){
+                sum += fast -> val;
             }
-            temp = temp->next;                    // ✅ move forward
+            else{ // fast -> val == 0
+                slow -> val = sum ;
+                newLastNode = slow ;
+                slow = slow -> next ;
+                sum = 0 ;
+            }
+            fast = fast -> next ;
+        } 
+
+        ListNode* temp = newLastNode -> next ; // Creating this one to delete unwanted nodes
+
+        // Just formed new List
+        newLastNode -> next = nullptr ;
+
+        // deleting unwanted list 
+        while(temp){
+            ListNode* nxt = temp -> next ;
+            delete temp ; 
+            temp = nxt ;
         }
-        
-        return dummy->next;
+        return head ;
     }
 };
