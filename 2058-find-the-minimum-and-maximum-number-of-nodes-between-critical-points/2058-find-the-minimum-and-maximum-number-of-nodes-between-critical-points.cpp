169af406a97c1af025e4157309c289b416d4bcd3
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-03-03 23:11:33 +0530

    Time: 3 ms (71.65%), Space: 124.7 MB (47.71%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -11,29 +11,49 @@
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-        vector<int> storeIndex ;
-        int index = 2 ;
+        vector<int> ans = {-1 , -1};
+
         ListNode* prev = head ;
+        if(!prev) return ans ;
+
         ListNode* curr = head -> next ;
-        while(curr -> next != nullptr){
-            if(curr -> val < prev -> val && curr -> val < curr -> next -> val){
-                storeIndex.push_back(index);
+        if(!head) return ans ;
+
+        ListNode* nxt  = head -> next -> next ;
+        if(!nxt) return ans ;
+
+        int firstCP = -1 ;
+        int lastCP = -1 ;
+        int minDist = INT_MAX ;
+        int i = 1 ;
+        while(nxt){
+            bool isCriticalPoint = ((curr -> val < prev -> val && curr -> val < nxt -> val) || 
+                                    (curr -> val > prev -> val && curr -> val > nxt -> val)) 
+                                        ? true : false ;
+            
+            // Found first Critical Point
+            if(isCriticalPoint && firstCP == -1){
+                firstCP = i  ;
+                lastCP = i ;
             }
-            else if(curr -> val > prev -> val && curr -> val > curr -> next -> val){
-                storeIndex.push_back(index);
+            else if(isCriticalPoint){
+                minDist = min(minDist , i - lastCP);
+                lastCP = i ;
             }
-            curr = curr -> next ;
+            ++i;
             prev = prev -> next ;
-            index++;
+            curr = curr -> next ;
+            nxt = nxt -> next ;
         }
 
-        if(storeIndex.size() < 2) return {-1 , -1} ;
-        int minIndex = INT_MAX  ;
-        for(int i = 1 ; i < storeIndex.size() ; i++){
-            int mini = storeIndex[i] - storeIndex[i-1];
-            minIndex = min(mini , minIndex);
+        if(lastCP == firstCP){
+            // Only one CP was found
+            return ans;
+        }else{
+            ans[0] = minDist ;
+            ans[1] = lastCP - firstCP ;
         }
 
-        return {minIndex , storeIndex[storeIndex.size() - 1] - storeIndex[0]};
+        return ans;
     }
 };
