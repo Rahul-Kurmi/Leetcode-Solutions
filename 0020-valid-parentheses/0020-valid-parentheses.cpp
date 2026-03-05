3f7abe6dcea53f9dfb07320e732a8d31511cf028
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-03-05 23:57:39 +0530

    Time: 0 ms (100%), Space: 9.1 MB (19.91%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -1,17 +1,29 @@
 class Solution {
 public:
     bool isValid(string s) {
+        unordered_map<char , char> mp = {
+            {'(' , ')'},
+            {'[' , ']'},
+            {'{' , '}'},
+        };
+
         stack<char> st ;
         for(int i = 0 ; i < s.size() ; i++){
-            if(s[i] == '(' || s[i] == '{' || s[i] == '['){
+            if(s[i] == '(' || s[i] == '[' || s[i] == '{'){
                 st.push(s[i]);
             }
-            else if(!st.empty() && s[i] == ')' && st.top() == '(') st.pop();
-            else if(!st.empty() && s[i] == ']' && st.top() == '[') st.pop();
-            else if(!st.empty() && s[i] == '}' && st.top() == '{') st.pop();
-            else st.push(s[i]); 
+            else if(!st.empty()){
+                char topVal = st.top();
+                if(mp[topVal] == s[i]){
+                    st.pop();
+                } else {
+                    return false; 
+                }
+            } else {
+                return false;  // ✅ closing bracket on empty stack
+            }
         }
 
-        return st.size() == 0 ? true : false ;
+        return st.empty() ;
     }
 };
