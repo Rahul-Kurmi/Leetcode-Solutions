17e9756d68a0d0ef9efec1d30e2ff10319a4b336
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-03-10 23:57:39 +0530

    Time: 7 ms (32.89%), Space: 11.8 MB (30.54%) - LeetHub

--- 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.cpp
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.cpp
@@ -1,16 +1,29 @@
-    class Solution {
-    public:
-        bool isValid(string &s) {
-            if(s == "") return true ;
-            int index = s.find("abc");
-
-            if(s.find("abc") != string::npos){
-                string left = s.substr(0 , index) ;
-                string right = s.substr(index + 3) ;
-                string newStr = left + right;
-                if(isValid(newStr)) return true ;
+class Solution {
+public:
+    bool isValid(string s) {
+        if(s[0] != 'a') return false ;
+        stack<char> st ;
+        for(char ch : s){
+            if(ch == 'a'){
+                st.push('a');
             }
-
-            return false ;
+            else if(ch == 'b'){
+                if(!st.empty() && st.top() == 'a'){
+                    st.push('b');
+                }
+                else return false ; 
+            }
+            else{
+                if(!st.empty() && st.top() == 'b'){
+                    st.pop();
+                    if(!st.empty() && st.top() == 'a'){
+                        st.pop();
+                    }
+                    else return false ;
+                }
+                else return false ;
+            }
         }
-    };
+        return st.empty();
+    }
+};
