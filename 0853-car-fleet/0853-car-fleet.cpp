755b04a839707f239c3c7698647d821877cb6a4e
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-03-12 22:03:13 +0530

    Time: 57 ms (40.31%), Space: 115.7 MB (17.44%) - LeetHub

---
+++ 0853-car-fleet/0853-car-fleet.cpp
@@ -0,0 +1,45 @@
+class Solution {
+    class Car{
+        public : 
+            int pos ;
+            int speed ;
+            Car(int _p , int _s) : pos(_p) , speed(_s) {} ;
+            static bool cmp(Car c1 , Car c2){
+                return c1.pos < c2.pos ;
+            }
+    };
+
+    
+
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        int n = position.size() ;
+        vector<Car> cars ;
+        for(int i = 0 ; i < n ; i++){
+            Car c1(position[i] , speed[i]);
+            cars.push_back(c1) ;
+        }
+
+        sort(cars.begin(), cars.end(), Car::cmp);
+
+        vector<double> times ;
+        for(int i = 0 ; i < n ; i++){
+            double time = (target - cars[i].pos)/((float)cars[i].speed);
+            times.push_back(time);
+        }
+
+        stack<double> st ;
+        for(int i = 0 ; i < n ; i++){
+            if(st.empty()){
+                st.push(times[i]);
+            }else{
+                while(!st.empty() && st.top() <= times[i]){
+                    st.pop();
+                }
+                st.push(times[i]);
+            }
+        }
+
+        return st.size();
+    }
+};
