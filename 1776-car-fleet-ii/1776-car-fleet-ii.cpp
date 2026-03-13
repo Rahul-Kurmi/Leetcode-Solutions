943cb808fd1029a7744e49b237afffdc586cc0d9
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-03-13 11:05:36 +0530

    Time: 16 ms (42.9%), Space: 135.4 MB (68.06%) - LeetHub

--- 1776-car-fleet-ii/1776-car-fleet-ii.cpp
+++ 1776-car-fleet-ii/1776-car-fleet-ii.cpp
@@ -2,36 +2,31 @@ class Solution {
 public:
     vector<double> getCollisionTimes(vector<vector<int>>& cars) {
         int n = cars.size() ;
-        vector<double> ans(n , -1) ;
+        vector<double> ans(n  , -1) ;
         stack<int> st ;
-        for(int i = n-1 ; i >= 0 ; i--){
-            while(!st.empty()){
-                // skip for those when currentCar cannot catch the topCar ie Car Ahead
-                int currSpeed = cars[i][1] ;
-                int stTopSpeed = cars[st.top()][1];
-                if(currSpeed <= stTopSpeed){
-                    st.pop();
-                    continue ; // As we have to check for next top with less speed than curr
-                }
-
-                // Now calculate time to reach the stTop car as skipped all faster cars now we are faster than the ahead one
-                double dist = cars[st.top()][0] - cars[i][0];
-                double speed = cars[i][1] - cars[st.top()][1];
-                double time = dist / speed ;
-
-                // Now if ahead car time = -1 ie it never collides with ahead car 
-                // or it's time is more than equal to current one thus this car will collide first 
-                if(ans[st.top()] == -1 || time <= ans[st.top()]){
-                    ans[i] = time ;
-                    break ; // don't go to nest iteration as found ans 
-                }
-
-                // pop as currentTime is more than achead time ie. ahead one collided first
-                // Now we have to check with the one it's collidex
+        for(int i = n - 1; i >= 0 ; i--){
+            // pop all the cars from top whose speed is more than current car
+            while(!st.empty() && cars[st.top()][1] >= cars[i][1]){
                 st.pop();
             }
 
-            // After this while loop we have found our ans --> push the current index for behind cars
+            // Now we got car with slow speed so check if time of stTop >= curr Car
+            while(!st.empty()){
+                double dist = cars[st.top()][0] - cars[i][0];
+                double speed = cars[i][1] - cars[st.top()][1] ;
+                double time = dist / speed ;
+                if(ans[st.top()] == -1 || time <= ans[st.top()]){
+                    // meean this collision happens 
+                    ans[i] = time ;
+                    break ; // we don't want next itration
+                } 
+
+                // we want next iteration as the time of ahead is less thus it collided first 
+                // so check for the next car ahead of this one
+                st.pop();
+            }
+
+            // push curr car index in stack for further cars behind
             st.push(i);
         }
 
