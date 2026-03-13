fb06f0da0193dadad531ed9b73fb86fded12c403
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-03-13 10:54:07 +0530

    Time: 17 ms (34.19%), Space: 135.4 MB (54.84%) - LeetHub

--- 1776-car-fleet-ii/1776-car-fleet-ii.cpp
+++ 1776-car-fleet-ii/1776-car-fleet-ii.cpp
@@ -1,26 +1,40 @@
 class Solution {
 public:
     vector<double> getCollisionTimes(vector<vector<int>>& cars) {
-        int n = cars.size();
-        vector<double> ans(n, -1);
-
-        for(int i = n-1; i >= 0; i--){
-            for(int j = i+1; j < n; j++){
-
-                if(cars[i][1] <= cars[j][1]) continue;
-
-                double relDist = cars[j][0] - cars[i][0];
-                double relSpeed = cars[i][1] - cars[j][1];
-                double time = relDist / relSpeed;
-
-                if(ans[j] == -1 || time <= ans[j]){
-                    ans[i] = time;
-                    break;
+        int n = cars.size() ;
+        vector<double> ans(n , -1) ;
+        stack<int> st ;
+        for(int i = n-1 ; i >= 0 ; i--){
+            while(!st.empty()){
+                // skip for those when currentCar cannot catch the topCar ie Car Ahead
+                int currSpeed = cars[i][1] ;
+                int stTopSpeed = cars[st.top()][1];
+                if(currSpeed <= stTopSpeed){
+                    st.pop();
+                    continue ; // As we have to check for next top with less speed than curr
                 }
-                if(time > ans[j]) continue ;
+
+                // Now calculate time to reach the stTop car as skipped all faster cars now we are faster than the ahead one
+                double dist = cars[st.top()][0] - cars[i][0];
+                double speed = cars[i][1] - cars[st.top()][1];
+                double time = dist / speed ;
+
+                // Now if ahead car time = -1 ie it never collides with ahead car 
+                // or it's time is more than equal to current one thus this car will collide first 
+                if(ans[st.top()] == -1 || time <= ans[st.top()]){
+                    ans[i] = time ;
+                    break ; // don't go to nest iteration as found ans 
+                }
+
+                // pop as currentTime is more than achead time ie. ahead one collided first
+                // Now we have to check with the one it's collidex
+                st.pop();
             }
+
+            // After this while loop we have found our ans --> push the current index for behind cars
+            st.push(i);
         }
 
-        return ans;
+        return ans ;
     }
 };
