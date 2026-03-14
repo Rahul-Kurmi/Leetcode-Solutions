8fe20d9352ef90b5214c16da015ec854ce3883dc
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-03-14 19:53:10 +0530

    Time: 0 ms (100%), Space: 23.7 MB (9.24%) - LeetHub

---
+++ 0622-design-circular-queue/0622-design-circular-queue.cpp
@@ -0,0 +1,91 @@
+class MyCircularQueue {
+public:
+    int* arr ;
+    int size ;
+    int front ;
+    int rear ;
+    MyCircularQueue(int k) {
+        arr = new int[k];
+        this -> size = k ;
+        front = -1 ;
+        rear = -1 ;
+    }
+    
+    bool enQueue(int value) {
+        if((rear == size-1  && front == 0) || (rear == front - 1)){
+            cout << "OverFlow" << endl ;
+            return false ;
+        }
+        else if(front == -1){
+            front++;
+            rear++;
+            arr[rear] = value ;
+        }
+        else if(rear == size - 1){
+            rear = 0;
+            arr[rear] = value ;
+        }
+        else{
+            rear++;
+            arr[rear] = value ;
+        }
+        return true ;
+    }
+    
+    bool deQueue() {
+        if(front == -1){
+            cout << "UnderFlow" << endl ;
+            return false ;
+        }
+        else if(front == rear){
+            // only one element in queue 
+            arr[front] = -1 ;
+            front = -1 ;
+            rear = -1;
+        }
+        else if(front == size - 1){
+            arr[front] = -1 ;
+            front = 0 ;
+        }
+        else{
+            arr[front] = -1 ;
+            front++;
+        }
+        return true ;
+    }
+    
+    int Front() {
+        if(front == -1){
+            return -1 ;
+        }
+        return arr[front];
+    }
+    
+    int Rear() {
+        if(front == -1){
+            return -1 ;
+        }
+        return arr[rear];
+    }
+    
+    bool isEmpty() {
+        return front == -1 ;
+    }
+    
+    bool isFull() {
+        if(front == 0 && rear == size - 1) return true ;
+        else if(rear == front - 1) return true ;
+        return false ;
+    }
+};
+
+/**
+ * Your MyCircularQueue object will be instantiated and called as such:
+ * MyCircularQueue* obj = new MyCircularQueue(k);
+ * bool param_1 = obj->enQueue(value);
+ * bool param_2 = obj->deQueue();
+ * int param_3 = obj->Front();
+ * int param_4 = obj->Rear();
+ * bool param_5 = obj->isEmpty();
+ * bool param_6 = obj->isFull();
+ */
