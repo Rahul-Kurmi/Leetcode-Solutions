5c5497e94fad2d5d391b96e7fec53e3d64017d0b
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-03-14 23:34:55 +0530

    Time: 4 ms (47.11%), Space: 24.5 MB (40.38%) - LeetHub

--- 0641-design-circular-deque/0641-design-circular-deque.cpp
+++ 0641-design-circular-deque/0641-design-circular-deque.cpp
@@ -12,26 +12,18 @@ public:
     }
     
     bool insertFront(int value) {
-        if((front == 0 && rear == size - 1) || (rear == front - 1)){
+        if((front - 1 + size ) % size == rear){
             return false;
         }
-
         // One Element 
         else if(front == -1){
             front++;
             arr[front] = value ;
             rear = front ;
         }
-
-        // front at zero 
-        else if(front == 0){
-            front = size - 1;
-            arr[front] = value ;
-        }
-
         // Rest front positions  
         else{
-            front--;
+            front = (front - 1 + size ) % size ;
             arr[front] = value ;
         }
 
@@ -39,29 +31,20 @@ public:
     }
     
     bool insertLast(int value) {
-        if((front == 0 && rear == size -1 ) || (rear == front - 1)){
+        if((rear + 1) % size == front){
             return false ;
         }
-
         // Empty Case 
         else if(front == -1){
             front++;
             arr[front] = value ;
             rear = front;
         }
-
-        // if rear == size - 1
-        else if(rear == size - 1){
-            rear = 0 ;
-            arr[rear] = value ;
-        }
-
         // rest case 
         else{
-            rear++;
+            rear = (rear + 1) % size ;
             arr[rear] = value ;
         }
-
         return true ;
     }
     
