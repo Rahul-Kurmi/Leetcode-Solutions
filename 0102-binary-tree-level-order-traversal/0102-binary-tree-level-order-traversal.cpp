37a359f35faf2cc27450ef5b1235cbd81dba48c3
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-03-20 23:52:46 +0530

    Time: 0 ms (100%), Space: 17.1 MB (71.5%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.cpp
@@ -9,58 +9,45 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+vector<vector<int>> levelOrderTraversal(TreeNode* root){
+    vector<vector<int>> ans ;
+    if (root == nullptr) {
+        return ans ;
+    }
+    
+    vector<int> store ;
+    queue<TreeNode*> q ;
+    // Push root node 
+    q.push(root);
+    q.push(nullptr);
+
+    while(!q.empty()){
+        TreeNode* node = q.front();
+        q.pop();
+
+        // If NULL → level finished
+        if(node == nullptr){
+            ans.push_back(store);
+            store.clear();
+
+            if(!q.empty()) q.push(nullptr);
+        }
+        else{
+            // Add current node value
+            store.push_back(node->val);
+
+            // Push children
+            if(node->left) q.push(node->left);
+            if(node->right) q.push(node->right);
+        }
+    }
+    return ans ;
+}
+
 
 class Solution {
 public:
     vector<vector<int>> levelOrder(TreeNode* root) {
-        vector<vector<int>> ans;   // Final result storing level-wise nodes
-        
-        // Edge case: if tree is empty
-        if(root == nullptr) return ans;
-        
-        queue<TreeNode*> q;
-        
-        // Push root and a NULL marker to indicate end of first level
-        q.push(root);
-        q.push(nullptr);
-
-        vector<int> store; // Stores nodes of current level
-        
-        while(!q.empty()){
-
-            // Add current node value to current level
-            store.push_back(q.front()->val);
-
-            // Push left child if exists
-            if(q.front()->left){
-                q.push(q.front()->left);
-            }
-
-            // Push right child if exists
-            if(q.front()->right){
-                q.push(q.front()->right);
-            }
-
-            // Remove current node from queue
-            q.pop();
-
-            // If next element is NULL → level completed
-            if(q.front() == nullptr){
-                
-                // Remove NULL marker
-                q.pop();
-
-                // Store current level in answer
-                ans.push_back(store);
-
-                // Clear for next level
-                store.clear();
-
-                // if queue empty then last level
-                if(!q.empty()) q.push(nullptr);
-            }
-        }
-        
-        return ans;
+        return levelOrderTraversal(root) ;
     }
 };
