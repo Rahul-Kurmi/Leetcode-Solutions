7af65ee569f5c8e7e291ecc04106ae553400e958
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-03-23 00:43:40 +0530

    Time: 3 ms (43.42%), Space: 21 MB (41.92%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -12,26 +12,34 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-    void pathSumHelper(TreeNode* root, int targetSum, vector<int> store){
-        if(root == nullptr) return ;
 
-        store.push_back(root -> val) ;
-        targetSum -= root -> val;
+    void pathSumHelper(TreeNode* root, int &targetSum, vector<int>& store){
+        if(root == nullptr) return;
 
-        if(root -> left == nullptr && root -> right == nullptr){
-            if(targetSum == 0) {
+        // choose
+        // HERE BOTH ARE PASS BY REFERENCE
+        store.push_back(root->val);
+        targetSum -= root->val;
+
+        // check leaf
+        if(root->left == nullptr && root->right == nullptr){
+            if(targetSum == 0){
                 ans.push_back(store);
-                return ;
             }
         }
 
-        pathSumHelper(root -> left , targetSum , store);
-        pathSumHelper(root -> right, targetSum , store);
+        // explore
+        pathSumHelper(root->left, targetSum, store);
+        pathSumHelper(root->right, targetSum, store);
+
+        // backtrack (VERY IMPORTANT)
+        store.pop_back();
+        targetSum += root->val;
     }
 
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        vector<int> store ;
-        pathSumHelper(root , targetSum , store);
+        vector<int> store;
+        pathSumHelper(root, targetSum, store);
         return ans;
     }
 };
