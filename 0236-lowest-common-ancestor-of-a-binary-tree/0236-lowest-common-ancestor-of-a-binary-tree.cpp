376bd9dd2ce86a2ca9b0f8d2d0a79c29bfaa77d1
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-03-22 22:33:03 +0530

    Time: 12 ms (66.13%), Space: 17.4 MB (37.76%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -10,21 +10,20 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root == nullptr) return nullptr ;
+        // Base case 
+        if(root == NULL) return NULL ;
 
-        if(root == p || root == q) return root ;
+        // check if node is p or q 
+        if(root == p) return p ;
+        if(root == q) return q ;
 
-        TreeNode* leftAns = lowestCommonAncestor(root -> left , p , q);
-        TreeNode* rightAns = lowestCommonAncestor(root -> right, p  , q);
+        TreeNode* left_ans = lowestCommonAncestor(root -> left , p , q) ;
+        TreeNode* right_ans = lowestCommonAncestor(root -> right , p , q);
 
-        // p and q found in different subtrees → current node is LCA
-        if(leftAns && rightAns) return root ;
-
-        // If only one side is non-null, return that side
-        // → Either:
-        //    1. One node (p or q) found in subtree
-        //    2. One node is ancestor of the other
-        return leftAns ? leftAns : rightAns ;
-
+        if(left_ans == NULL && right_ans == NULL) return NULL ;
+        else if(left_ans != NULL && right_ans == NULL) return left_ans;
+        else if(left_ans == NULL && right_ans != NULL) return right_ans ;
+        // if(left_ans != NULL && right_ans != NULL)
+        else return root ;
     }
 };
