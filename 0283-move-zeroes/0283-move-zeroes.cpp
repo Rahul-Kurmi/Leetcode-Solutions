fec3b448c29483ee57a6ff7f95ee15470d6db7b5
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-03-24 19:50:44 +0530

    Time: 0 ms (100%), Space: 23.8 MB (55.14%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.cpp
+++ 0283-move-zeroes/0283-move-zeroes.cpp
@@ -6,17 +6,10 @@ public:
         while(iterateZero < nums.size()){
             if(nums[iterateZero] == 0) iterateZero++;
             else{
-                // Here we placed all the non zeros in right side
-                // but this is copy of all non zeros  
-                nums[index] = nums[iterateZero];
+                swap(nums[index] , nums[iterateZero]);
                 iterateZero++;
                 index++;
             }
-        }
-
-        // After index we have to make all zeros
-        while(index < nums.size()) {
-            nums[index++] = 0 ;
         }
     }
 };
