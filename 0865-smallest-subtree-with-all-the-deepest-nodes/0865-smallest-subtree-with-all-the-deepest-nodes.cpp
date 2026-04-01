8a30ca83a9b24ac9ed278d43db4b0ccb3825e5bc
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-04-02 00:20:42 +0530

    Time: 0 ms (100%), Space: 16.3 MB (48.77%) - LeetHub

---
+++ 0865-smallest-subtree-with-all-the-deepest-nodes/0865-smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    pair<int , TreeNode*> lcaDeepestLeaveshelper(TreeNode* root){
+        if(!root) return { 0 , nullptr} ;
+
+        auto leftAns = lcaDeepestLeaveshelper(root -> left);
+        auto rightAns = lcaDeepestLeaveshelper(root -> right);
+
+        int leftDepth = leftAns.first ;
+        int rightDepth = rightAns.first ;
+        if(leftDepth == rightDepth){
+            return {leftDepth + 1 , root};
+        }
+        if(leftDepth > rightDepth){
+            return {leftDepth + 1 , leftAns.second};
+        }
+        if(rightDepth > leftDepth){
+            return {rightDepth + 1 , rightAns.second};
+        }
+        return {0 , nullptr} ;
+    }
+
+    TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+        auto pairHeightLCA = lcaDeepestLeaveshelper(root);
+        return pairHeightLCA.second ;
+    }
+};
