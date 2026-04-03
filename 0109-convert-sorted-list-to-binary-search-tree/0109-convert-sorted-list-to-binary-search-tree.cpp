a556443ce3fccdb256a09962f088447d931419b3
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-04-04 00:35:23 +0530

    Time: 3 ms (96.34%), Space: 33.1 MB (56.78%) - LeetHub

--- 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
+++ 0109-convert-sorted-list-to-binary-search-tree/0109-convert-sorted-list-to-binary-search-tree.cpp
@@ -1,41 +1,52 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    ListNode* midAndPrev(ListNode* head){
-        ListNode* slow = head;
-        ListNode* fast = head;
-        ListNode* prev = nullptr;
+    TreeNode* SortedLL_Into_BST(ListNode*& head, int n) {
+        if(n <= 0 || !head ) return nullptr ;
 
-        while(fast && fast->next){
-            prev = slow;
-            slow = slow->next;
-            fast = fast->next->next;
-        }
+        TreeNode* leftSubTree = SortedLL_Into_BST(head , n - n/2 - 1);
 
-        // break left half
-        if(prev) prev->next = nullptr;
+        TreeNode* root = new TreeNode(head -> val) ;
 
-        return slow; // mid
+        root -> left = leftSubTree ;
+        head = head -> next ;
+
+        root -> right = SortedLL_Into_BST(head , n/2);
+        return root ; 
     }
 
-    TreeNode* solve(ListNode* head){
-        if(!head) return nullptr;
-
-        // ✅ handle single node early
-        if(head->next == nullptr){
-            return new TreeNode(head->val);
+    int sizeOfLL(ListNode* head) {
+        ListNode* temp = head;
+        int count = 0;
+        while (temp != nullptr) {
+            count++;
+            temp = temp->next;
         }
-
-        ListNode* mid = midAndPrev(head);
-
-        TreeNode* root = new TreeNode(mid->val);
-
-        root->left = solve(head);
-        root->right = solve(mid->next);
-
-        return root;
+        return count;
     }
 
     TreeNode* sortedListToBST(ListNode* head) {
-        return solve(head);
+        int n = sizeOfLL(head);
+        return SortedLL_Into_BST(head, n);
     }
 };
