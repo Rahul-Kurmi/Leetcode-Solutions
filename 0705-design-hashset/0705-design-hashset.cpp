6e1b54b78ac088f952b0d37ab11f358c3036a1ad
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-04-18 02:03:54 +0530

    Time: 11 ms (76.31%), Space: 50 MB (64.35%) - LeetHub

--- 0705-design-hashset/0705-design-hashset.cpp
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -1,27 +1,82 @@
-class MyHashSet {
+class Node {
 public:
-    vector<bool> vec;
-    MyHashSet() {
-        vec.resize(1000001 , false);
-    }
-    
-    void add(int key) {
-        vec[key] = true ;
-    }
-    
-    void remove(int key) {
-        vec[key] = false ;
-    }
-    
-    bool contains(int key) {
-        return vec[key];
+    int data;
+    Node* next;
+
+    Node(int _d) {
+        data = _d;
+        next = nullptr;
     }
 };
 
-/**
- * Your MyHashSet object will be instantiated and called as such:
- * MyHashSet* obj = new MyHashSet();
- * obj->add(key);
- * obj->remove(key);
- * bool param_3 = obj->contains(key);
- */
+class MyHashSet {
+public:
+    vector<Node*> arr;
+    static int m;
+
+    static int hash(int key) {
+        return key % m;
+    }
+
+    MyHashSet() {
+        m = 10007;
+        arr.resize(m, nullptr);
+    }
+
+    void add(int key) {
+        int index = hash(key);
+
+        Node* temp = arr[index];
+
+        // check duplicates
+        while (temp != nullptr) {
+            if (temp->data == key) return;
+            if (temp->next == nullptr) break;
+            temp = temp->next;
+        }
+
+        Node* newNode = new Node(key);
+
+        if (arr[index] == nullptr) {
+            arr[index] = newNode;
+        } else {
+            temp->next = newNode;
+        }
+    }
+
+    void remove(int key) {
+        int index = hash(key);
+
+        Node* temp = arr[index];
+        Node* prev = nullptr;
+
+        while (temp != nullptr) {
+            if (temp->data == key) {
+                if (prev == nullptr) {
+                    arr[index] = temp->next;
+                } else {
+                    prev->next = temp->next;
+                }
+                delete temp;
+                return; // important
+            }
+            prev = temp;
+            temp = temp->next;
+        }
+    }
+
+    bool contains(int key) {
+        int index = hash(key);
+
+        Node* temp = arr[index];
+
+        while (temp != nullptr) {
+            if (temp->data == key) return true;
+            temp = temp->next;
+        }
+
+        return false;
+    }
+};
+
+int MyHashSet::m = 0;
