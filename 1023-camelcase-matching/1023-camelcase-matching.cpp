294bca9f259aa8247b3c9bd74e0607ac0812a8b8
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-04-24 22:43:09 +0530

    Time: 5 ms (1.83%), Space: 18.4 MB (5.03%) - LeetHub

--- 1023-camelcase-matching/1023-camelcase-matching.cpp
+++ 1023-camelcase-matching/1023-camelcase-matching.cpp
@@ -1,30 +1,57 @@
+class TrieNode{
+    public:
+        char data ;
+        TrieNode* children[256];
+        bool isTerminal;
+
+        TrieNode(char ch){
+            this -> data = ch;
+            for(int i = 0 ; i < 256 ; i++){
+                children[i] = nullptr ;
+            }
+            this -> isTerminal = false;
+        }
+};
+
 class Solution {
 public:
-
-    bool camelMatchHelper(string word , string pattern){
-        int i = 0 ;
-        int j = 0 ;
-        while(i < word.size()){
-            if(word[i] == pattern[j]){
-                i++;
-                j++;
-            }
-            else if(islower(word[i])){
-                i++;
-            }
-            else{
-                return false;
-            }
+    void insertUtil(TrieNode* root, string word , int i){
+        if(i == word.length()){
+            root -> isTerminal = true ;
+            return ;
         }
 
-        return j == pattern.size();
+        int index = word[i];
+        if(!root -> children[index]){
+            root -> children[index] = new TrieNode(word[i]);
+        }
+
+        insertUtil(root -> children[index] , word , i+1);
+    }
+
+
+    bool isCamelMatch(TrieNode* root , string word, int i){
+        if(i == word.size()){
+            return root->isTerminal; // ✅ ensure full match
+        }
+
+        int index = word[i];
+        if(root -> children[index]){
+            return isCamelMatch(root -> children[index] , word, i+1);
+        }
+        else if(islower(word[i])){
+            return isCamelMatch(root , word, i+1);
+        }
+        return false;
     }
 
     vector<bool> camelMatch(vector<string>& queries, string pattern) {
+        TrieNode* root = new TrieNode('-');
+        insertUtil(root , pattern , 0);
         vector<bool> ans ;
         for(auto word : queries){
-            ans.push_back(camelMatchHelper(word , pattern));
+            ans.push_back(isCamelMatch(root , word, 0));
         }
-        return ans;
+        return ans ;
     }
 };
