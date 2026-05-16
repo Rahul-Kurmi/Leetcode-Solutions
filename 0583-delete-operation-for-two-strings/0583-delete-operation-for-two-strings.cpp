12a3f5b7ce483bbebea7ad74669f27eddecaea47
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-05-17 01:18:28 +0530

    Time: 1 ms (99.04%), Space: 11.4 MB (92.98%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -4,21 +4,22 @@ public:
         int m = word1.size();
         int n = word2.size();
         
-        vector<vector<int>> dp(m+1 , vector<int>(n+1 , 0));
+        // optimized tabulation
+        vector<int> prev(n+1, 0) , curr(n+1 , 0);
 
-        // JUST TABULATION TO FIND LCS
         for(int i = 1 ; i <= m ; i++){
             for(int j = 1 ; j <= n ; j++){
                 if(word1[i-1] == word2[j-1]){
-                    dp[i][j] = 1 + dp[i-1][j-1];
+                    curr[j] = 1 + prev[j-1];
                 }
                 else{
-                    dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
+                    curr[j] = max(prev[j] , curr[j-1]);
                 }
             }
+            prev = curr ;
         }
 
-        int matchLen = dp[m][n];
+        int matchLen = prev[n];
 
         int ans = (m-matchLen) + (n - matchLen);
         return ans ;
