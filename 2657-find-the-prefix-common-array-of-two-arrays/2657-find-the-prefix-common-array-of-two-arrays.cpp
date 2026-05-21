66f1fe3a9d6c4aed2d1b4227cf86bf8a5775faee
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-05-22 00:12:39 +0530

    Time: 12 ms (36.07%), Space: 88 MB (40.56%) - LeetHub

--- 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.cpp
+++ 2657-find-the-prefix-common-array-of-two-arrays/2657-find-the-prefix-common-array-of-two-arrays.cpp
@@ -1,32 +1,15 @@
 class Solution {
 public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
-        int n = A.size();
-
-        // take 2 boolean array pf size -> n+1
-        vector<bool> isPresentInA(n+1 , false);
-        vector<bool> isPresentInB(n+1 , false);
-        // these will mark for each element is present till ith iteration
-        // size -> n+1 because element 1 -> n in arr
-        // thus bool index 0 -> n to mark every elements 
-
-        vector<int> ans(n);
-
-        // SC : O(n) --> 2 , 2 extra boolen array 
-        // TC : O(n^2)
-
-        for(int i = 0 ; i < n ; i++){
-            isPresentInA[A[i]] = true ;
-            isPresentInB[B[i]] = true ;
-
-            int matched = 0 ;
-            for(int check = 1 ; check <= n ; check++){
-                if(isPresentInA[check] && isPresentInB[check]){
-                    matched++;
-                }
+        unordered_set<int> st ;
+        vector<int> ans(A.size()) ;
+        for(int i = 0 ; i < B.size(); i++){
+            int foundVal = 0;
+            st.insert(A[i]);
+            for(int j = 0; j <= i ; j++){
+                if(st.count(B[j])) foundVal++;
             }
-
-            ans[i] = matched ;
+            ans[i] = foundVal;
         }
         return ans ;
     }
