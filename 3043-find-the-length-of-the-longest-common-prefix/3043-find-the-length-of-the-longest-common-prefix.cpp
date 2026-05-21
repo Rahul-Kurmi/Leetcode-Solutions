446dadd22fdf726a054a61affa30090d93f4a75b
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-05-22 00:21:58 +0530

    Time: 151 ms (64%), Space: 134.7 MB (91.83%) - LeetHub

--- 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.cpp
+++ 3043-find-the-length-of-the-longest-common-prefix/3043-find-the-length-of-the-longest-common-prefix.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
-        unordered_map<string, bool> mp;
-        for(auto &it: arr1){
-            string str = to_string(it);
-            string store = "";
-            for(int i = 0 ; i < str.size() ; i++){
-                store += str[i];
-                mp[store] = true;
+        unordered_set<int> st; // store the prefix of every element in arr1
+        
+        // eg :- 1234 -> {1234, 123, 12, 1}
+        for(auto val: arr1){
+            // if val is not present and it's value is > 0 then insert in set
+            while(!st.count(val) && val > 0){
+                st.insert(val);
+                val = val / 10 ;
             }
         }
 
-        int maxPrefix = 0;
-        for(auto &it: arr2){
-            string str = to_string(it);
-            string store = "";
-            for(int i = 0 ; i < str.size() ; i++){
-                store += str[i];
-                if(mp.count(store)){
-                    maxPrefix = max(maxPrefix, (int)store.size());
-                }
+
+        int result = 0 ; // Longest common Prefix
+        for(auto val : arr2){
+            // resduce val until it's present in set
+            while(!st.count(val) && val > 0){
+                val = val / 10 ;
+            }
+
+            // now this reduced val is in the set thus take it's length
+            // length of a integer digit --> log10(num) + 1
+            if(val > 0){
+                result = max(result , (int)log10(val) + 1);
             }
         }
-        return maxPrefix;
+        return result ;
     }
 };
