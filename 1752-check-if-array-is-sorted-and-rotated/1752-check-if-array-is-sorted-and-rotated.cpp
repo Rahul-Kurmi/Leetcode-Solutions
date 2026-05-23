ea0fc51f5ccc6d95bcc8a4c51527ef7c2e3aa4d7
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-05-23 23:52:44 +0530

    Time: 0 ms (100%), Space: 11.9 MB (5.2%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,4 +1,4 @@
-//Approach-1 (Super Brute Force)
+//Approach-2 (Better Brute Force)
 //T.C : O(n^2)
 //S.C : O(n)
 class Solution {
@@ -6,25 +6,13 @@ public:
     bool check(vector<int>& nums) {
         int n = nums.size();
 
-        vector<int> sorted(n);
+        vector<int> sorted = nums;
+        sort(begin(sorted), end(sorted));
 
         for(int r = 0; r < n; r++) {
-
-            int idx = 0;
-            for(int i = r; i < n; i++) {
-                sorted[idx] = nums[i];
-                idx++;
-            }
-
-            for(int i = 0; i < r; i++) {
-                sorted[idx] = nums[i];
-                idx++;
-            }
-
-            //check if sorted;
             bool isSorted = true;
-            for(int i = 0; i < n-1; i++) {
-                if(sorted[i] > sorted[i+1]) {
+            for(int i = 0; i < n; i++) {
+                if(sorted[i] != nums[(i + r)%n]) {
                     isSorted = false;
                     break;
                 }
