3270dbdae9e476352a39717099d745e27e886742
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-05-25 15:59:56 +0530

    Time: 20 ms (82.75%), Space: 9.7 MB (94.98%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -4,28 +4,29 @@ public:
         string t = s ;
         reverse(t.begin() , t.end());
         int n = s.size();
-        vector<vector<int>> dp(n+1 , vector<int>(n+1, 0));
+        vector<int> prev(n+1, 0);
+        vector<int> curr(n+1, 0);
 
         // BASE CASE
-        for(int i = 0 ; i < n ; i++) dp[i][0] = 0;
-        for(int j = 0 ; j < n ; j++) dp[0][j] = 0;
-
+        for(int i = 0 ; i <= n ; i++) prev[i] = 0;
+        
         for(int i = 1 ; i <= n ; i++){
             for(int j = 1 ; j <= n ; j++){
                 int result = 0 ;
                 if(s[i - 1] == t[j - 1]){
-                    result = 1 + dp[i-1][j-1];
+                    result = 1 + prev[j-1];
                 }
                 else{
-                    result = max(dp[i][j-1] , dp[i-1][j]);
+                    result = max(curr[j-1] , prev[j]);
                 }
 
-                dp[i][j] =  result ;
+                curr[j] =  result ;
             }
+            prev = curr ;
         }
 
 
-        int longPallSubsequence = dp[n][n];
+        int longPallSubsequence = prev[n];
         return n - longPallSubsequence ;
     }
 };
