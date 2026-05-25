d04bc2f58fd6f06fc06dfeb2fc8e78eef416374a
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-05-25 15:57:34 +0530

    Time: 31 ms (64.78%), Space: 31.5 MB (59.51%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,28 +1,31 @@
 class Solution {
 public:
-
-    int lcs(string& s , string& t , int i , int j , vector<vector<int>>& dp){
-        if(i == 0 || j == 0) return 0;
-
-        if(dp[i][j] != -1) return dp[i][j];
-        
-        int result = 0 ;
-        if(s[i - 1] == t[j - 1]){
-            result = 1 + lcs(s , t, i - 1, j - 1, dp);
-        }
-        else{
-            result = max(lcs(s, t, i, j - 1, dp) , lcs(s, t, i- 1, j, dp));
-        }
-
-        return dp[i][j] =  result ;
-    }
-
     int minInsertions(string s) {
         string t = s ;
         reverse(t.begin() , t.end());
         int n = s.size();
-        vector<vector<int>> dp(n+1 , vector<int>(n+1, -1));
-        int longPallSubsequence = lcs(s , t, n, n, dp);
+        vector<vector<int>> dp(n+1 , vector<int>(n+1, 0));
+
+        // BASE CASE
+        for(int i = 0 ; i < n ; i++) dp[i][0] = 0;
+        for(int j = 0 ; j < n ; j++) dp[0][j] = 0;
+
+        for(int i = 1 ; i <= n ; i++){
+            for(int j = 1 ; j <= n ; j++){
+                int result = 0 ;
+                if(s[i - 1] == t[j - 1]){
+                    result = 1 + dp[i-1][j-1];
+                }
+                else{
+                    result = max(dp[i][j-1] , dp[i-1][j]);
+                }
+
+                dp[i][j] =  result ;
+            }
+        }
+
+
+        int longPallSubsequence = dp[n][n];
         return n - longPallSubsequence ;
     }
 };
