fc11cb75b037d6640f829c4d341d121a7432eb91
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-06-08 22:47:38 +0530

    Time: 11 ms (30.45%), Space: 127.6 MB (98.06%) - LeetHub

---
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.cpp
@@ -0,0 +1,32 @@
+//Approach-3 (Using pointers only)
+//T.C : O(n)
+//S.C : O(n) or O(1) if ans not included
+class Solution {
+public:
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        int n = nums.size();
+        vector<int> ans(n);
+        int left = 0 ;
+        int right = n - 1;
+        int i = 0 ;
+        int j = n-1 ;
+        while(i < n){
+            if(nums[i] < pivot){
+                ans[left++] = nums[i];
+            }
+
+            if(nums[j] > pivot){
+                ans[right--] = nums[j];
+            }
+
+            i++;
+            j--;
+        }
+
+        while(left <= right){
+            ans[left++] = pivot;
+        }
+
+        return ans ;
+    }
+}; 
