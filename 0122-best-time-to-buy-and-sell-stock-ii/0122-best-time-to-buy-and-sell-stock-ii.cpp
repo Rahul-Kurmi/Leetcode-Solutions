fc6652d12b7dc23004e47c9be43dc134a605db9d
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-06-14 01:47:58 +0530

    Time: 0 ms (100%), Space: 20.1 MB (60.91%) - LeetHub

---
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+
+        // SPACE OPTIMIZED 
+        vector<int>next (2, 0);
+        vector<int>curr (2, 0);
+
+        for(int i = n-1 ; i >= 0 ; i--){
+            for(int buy = 0 ; buy <= 1 ; buy++){
+                int profit ;
+                if(buy){
+                    int buyProfit = -prices[i] + next[0];
+                    int skipProfit = 0 + next[1];
+                    profit = max(buyProfit, skipProfit);
+                }
+                else{ // buy = 0, means can sell only
+                    int sellProfit = prices[i] + next[1];
+                    int skipProfit = 0 + next[0];
+                    profit = max(sellProfit , skipProfit);
+                }
+
+                curr[buy] = profit ;
+            }
+            next = curr ;
+        }
+
+        return next[1];
+    }
+};
