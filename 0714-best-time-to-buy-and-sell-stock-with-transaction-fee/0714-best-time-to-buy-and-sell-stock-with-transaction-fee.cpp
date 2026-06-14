e98316f3138d94c3542fe5e0a17f7a40d191952c
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-06-14 23:26:37 +0530

    Time: 18 ms (64.92%), Space: 63.5 MB (89.83%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,49 +1,31 @@
 class Solution {
 public:
-    // PRE-REQUISITE :- Buy And Sell 2
-    int maxProfitHelper(vector<int>& prices, int i, int buy, int& fee, vector<vector<int>>& dp){
-        if(i == prices.size()) return 0 ;
-
-        if(dp[i][buy] != -1) return dp[i][buy];
-
-        int profit ;
-        if(buy){
-            int buyProfit = -prices[i] + maxProfitHelper(prices, i+1, 0 , fee, dp);
-            int skipProfit = 0 + maxProfitHelper(prices, i+1, 1, fee, dp);
-            profit = max(buyProfit , skipProfit);
-        }
-        else{ // we're paying transaction fee at each full transaction
-            int sellProfit = -fee + prices[i] + maxProfitHelper(prices, i+1, 1, fee, dp);
-            int skipProfit = 0 + maxProfitHelper(prices, i+1, 0 , fee, dp);
-            profit = max(sellProfit , skipProfit);
-        }
-
-        return dp[i][buy]=  profit ;
-    }
-
     int maxProfit(vector<int>& prices, int fee) {
         int n = prices.size();
 
-        // TABULATION CODE
-        vector<vector<int>> dp(n+1 , vector<int>(2, 0));
+        // SPACE OPTIMIZATION
+        vector<int> next(2, 0);
+        vector<int> curr(2, 0);
+
         for(int i = n-1 ; i >= 0 ; i--){
             for(int buy = 0 ; buy <= 1 ; buy++){
                 int profit ;
                 if(buy){
-                    int buyProfit = -prices[i] + dp[i+1][0];
-                    int skipProfit = 0 + dp[i+1][1];
+                    int buyProfit = -prices[i] + next[0];
+                    int skipProfit = 0 + next[1];
                     profit = max(buyProfit , skipProfit);
                 }
                 else{ // we're paying transaction fee at each full transaction
-                    int sellProfit = -fee + prices[i] + dp[i+1][1];
-                    int skipProfit = 0 + dp[i+1][0];
+                    int sellProfit = -fee + prices[i] + next[1];
+                    int skipProfit = 0 + next[0];
                     profit = max(sellProfit , skipProfit);
                 }
 
-                dp[i][buy]=  profit ;
+                curr[buy]=  profit ;
             }
+            next = curr;
         }
 
-        return dp[0][1];
+        return next[1];
     }
 };
