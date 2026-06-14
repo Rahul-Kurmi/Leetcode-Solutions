bd24a17583a03df7cb3ee2e94418f3ebb2692b95
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-06-15 00:56:56 +0530

    Time: 0 ms (100%), Space: 15 MB (85.16%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -2,29 +2,32 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n+2 , vector<int>(2, 0));
-        // TABULATION CODE
-
-        // BASE CASE dp[n] = 0 ; no need to write as all initialized with 0
+        // vector<vector<int>> dp(n+2 , vector<int>(2, 0));
+        // as we have i+1,i+2 --> need 3, 1D vector for space optimized
+        vector<int> nextNext(2, 0);
+        vector<int> next(2, 0);
+        vector<int> curr(2, 0);  
 
         for(int i = n-1 ; i >= 0 ; i--){
             for(int buy = 0 ; buy <= 1 ; buy++){
                 int profit ;
                 if(buy){
-                    int buyProfit = -prices[i] + dp[i+1][0];
-                    int skipProfit = 0 + dp[i+1][1];
+                    int buyProfit = -prices[i] + next[0];
+                    int skipProfit = 0 + next[1];
                     profit = max(buyProfit, skipProfit);
                 }
                 else{ // buy = 0, means can sell only
-                    int sellProfit = prices[i] + dp[i+2][1];
-                    int skipProfit = 0 + dp[i+1][0];
+                    int sellProfit = prices[i] + nextNext[1];
+                    int skipProfit = 0 + next[0];
                     profit = max(sellProfit , skipProfit);
                 }
 
-                dp[i][buy] = profit ;
+                curr[buy] = profit ;
             }
+            nextNext = next ;
+            next = curr; 
         }
 
-        return dp[0][1];
+        return next[1];
     }
 };
