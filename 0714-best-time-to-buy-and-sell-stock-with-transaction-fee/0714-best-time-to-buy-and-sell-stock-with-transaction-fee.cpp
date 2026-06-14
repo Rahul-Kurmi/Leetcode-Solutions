ac57f0764aeea16eadf8eed293dc66e47a53cb80
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-06-14 23:26:47 +0530

    Time: 0 ms (100%), Space: 63.4 MB (89.83%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -3,29 +3,22 @@ public:
     int maxProfit(vector<int>& prices, int fee) {
         int n = prices.size();
 
-        // SPACE OPTIMIZATION
-        vector<int> next(2, 0);
-        vector<int> curr(2, 0);
+        // SPACE OPTIMIZATION with 4 VARIABLES
+
+        int nextBuy = 0 , nextNotBuy = 0 ;
+        int currBuy , currNotBuy ;
 
         for(int i = n-1 ; i >= 0 ; i--){
-            for(int buy = 0 ; buy <= 1 ; buy++){
-                int profit ;
-                if(buy){
-                    int buyProfit = -prices[i] + next[0];
-                    int skipProfit = 0 + next[1];
-                    profit = max(buyProfit , skipProfit);
-                }
-                else{ // we're paying transaction fee at each full transaction
-                    int sellProfit = -fee + prices[i] + next[1];
-                    int skipProfit = 0 + next[0];
-                    profit = max(sellProfit , skipProfit);
-                }
+            
+            // buy case
+            currBuy = max(-prices[i] + nextNotBuy , 0 + nextBuy);
+            // sell case
+            currNotBuy = max(-fee + prices[i] + nextBuy , 0 + nextNotBuy);
 
-                curr[buy]=  profit ;
-            }
-            next = curr;
+            nextBuy = currBuy;
+            nextNotBuy = currNotBuy;
         }
 
-        return next[1];
+        return nextBuy;
     }
 };
