a6aff94e7f954bccbb5254f5b388ee27dd70c9c7
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-06-15 00:56:43 +0530

    Time: 4 ms (19.33%), Space: 16.2 MB (34.6%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,30 +1,30 @@
 class Solution {
 public:
-    int maxProfitHelper(vector<int>& prices, int i, int buy, vector<vector<int>>& dp){
-        if(i >= prices.size()) {
-            return 0 ;
-        }
-
-        if(dp[i][buy] != -1) return dp[i][buy];
-
-        int profit ;
-        if(buy){
-            int buyProfit = -prices[i] + maxProfitHelper(prices , i+1 , 0, dp);
-            int skipProfit = 0 + maxProfitHelper(prices, i+1 , 1, dp);
-            profit = max(buyProfit, skipProfit);
-        }
-        else{ // buy = 0, means can sell only
-            int sellProfit = prices[i] + maxProfitHelper(prices, i+2 , 1, dp);
-            int skipProfit = 0 + maxProfitHelper(prices , i+1 , 0, dp);
-            profit = max(sellProfit , skipProfit);
-        }
-
-        return  dp[i][buy] = profit ;
-    }
-
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n , vector<int>(2, -1)); 
-        return maxProfitHelper(prices, 0 , 1, dp); // start buy = 1 as we must buy in start to sell
+        vector<vector<int>> dp(n+2 , vector<int>(2, 0));
+        // TABULATION CODE
+
+        // BASE CASE dp[n] = 0 ; no need to write as all initialized with 0
+
+        for(int i = n-1 ; i >= 0 ; i--){
+            for(int buy = 0 ; buy <= 1 ; buy++){
+                int profit ;
+                if(buy){
+                    int buyProfit = -prices[i] + dp[i+1][0];
+                    int skipProfit = 0 + dp[i+1][1];
+                    profit = max(buyProfit, skipProfit);
+                }
+                else{ // buy = 0, means can sell only
+                    int sellProfit = prices[i] + dp[i+2][1];
+                    int skipProfit = 0 + dp[i+1][0];
+                    profit = max(sellProfit , skipProfit);
+                }
+
+                dp[i][buy] = profit ;
+            }
+        }
+
+        return dp[0][1];
     }
 };
