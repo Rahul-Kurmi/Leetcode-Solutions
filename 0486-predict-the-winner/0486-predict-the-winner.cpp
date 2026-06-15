9340374d48b7ed21c111d0106ab7fb061dfd5590
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-06-16 02:47:24 +0530

    Time: 2 ms (41.65%), Space: 10.3 MB (12.59%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int start , int end){
+    int solve(vector<int>& nums, int start , int end, vector<vector<int>>& dp){
         if(start == end) return nums[start];
 
-        // diff = P1 - P2
-        int diffByStart = nums[start] - solve(nums, start+1 , end);
-        int diffByEnd = nums[end] - solve(nums, start, end-1);
+        if(dp[start][end] != -1) return dp[start][end];
 
-        return max(diffByStart , diffByEnd);
+        // diff = P1 - P2
+        int diffByStart = nums[start] - solve(nums, start+1 , end, dp);
+        int diffByEnd = nums[end] - solve(nums, start, end-1, dp);
+
+        return dp[start][end] = max(diffByStart , diffByEnd);
     }
 
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        return solve(nums, 0 , n-1) >= 0;
+        vector<vector<int>> dp(n+1 , vector<int>(n+1, -1));
+        return solve(nums, 0 , n-1, dp) >= 0;
     }
 };
