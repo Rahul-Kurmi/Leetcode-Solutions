d0f3a65e08df5082848fa5d357a826f8de89a4e3
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-06-16 02:47:14 +0530

    Time: 91 ms (21.82%), Space: 9.6 MB (86.95%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,27 +1,17 @@
 class Solution {
 public:
-    int solveForP1(vector<int>& nums , int i, int j, vector<vector<int>>& dp){
-        if(i > j) return 0 ;
-        if(i == j) return nums[i];
+    int solve(vector<int>& nums, int start , int end){
+        if(start == end) return nums[start];
 
-        if(dp[i][j] != -1) return dp[i][j];
+        // diff = P1 - P2
+        int diffByStart = nums[start] - solve(nums, start+1 , end);
+        int diffByEnd = nums[end] - solve(nums, start, end-1);
 
-        int take_i = nums[i] + min(solveForP1(nums, i+2 , j, dp), solveForP1(nums, i+1, j-1, dp));
-        int take_j = nums[j] + min(solveForP1(nums, i+1, j-1, dp), solveForP1(nums, i, j-2, dp));
-
-        return dp[i][j] =  max(take_i , take_j);
+        return max(diffByStart , diffByEnd);
     }
 
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
-
-        int totalSum = accumulate(nums.begin() , nums.end() , 0);
-        
-        vector<vector<int>> dp(n+1, vector<int> (n+1, -1));
-
-        int player1Sum = solveForP1(nums, 0 , n-1, dp);
-        int player2Sum = totalSum - player1Sum ;
-
-        return player1Sum >= player2Sum ;
+        return solve(nums, 0 , n-1) >= 0;
     }
 };
