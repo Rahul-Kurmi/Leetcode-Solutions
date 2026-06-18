d6fe0b7b1e97bdfe06754e06d9113d865045c06d
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-06-19 04:23:16 +0530

    Time: 104 ms (21.2%), Space: 136.9 MB (28.19%) - LeetHub

--- 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
+++ 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -8,11 +8,7 @@ public:
         int swap = INT_MAX , noSwap = INT_MAX ;
 
         int prev1 , prev2 ; 
-        if(i == 0){ // this is neceasry as when i = 0, we can't acces (i-1)th index
-            prev1 = -1;
-            prev2 = -1;
-        }
-        else if(isSwap){
+        if(isSwap){
             prev1 = nums2[i-1];
             prev2 = nums1[i-1];
         }
@@ -35,40 +31,12 @@ public:
     }
 
     int minSwap(vector<int>& nums1, vector<int>& nums2) {
+        nums1.insert(nums1.begin() , -1);
+        nums2.insert(nums2.begin() , -1);
+
         int n = nums1.size();
-        vector<vector<int>> dp(n + 1 , vector<int>(2, 0));
-        // TABULATION CODE 
-
-        for(int i = n - 1 ; i >= 0 ; i--){
-            for(int isSwap = 1; isSwap >= 0 ; isSwap--){
-                int swap = INT_MAX , noSwap = INT_MAX ;
-                int prev1 , prev2 ; 
-                if(i == 0){ // this is neceasry as when i = 0, we can't acces (i-1)th index
-                    prev1 = -1;
-                    prev2 = -1;
-                }
-                else if(isSwap){
-                    prev1 = nums2[i-1];
-                    prev2 = nums1[i-1];
-                }
-                else{
-                    prev1 = nums1[i-1];
-                    prev2 = nums2[i-1];
-                }
-
-
-                if(prev1 < nums2[i] && prev2 < nums1[i]){
-                    swap = 1 + dp[i+1][1];
-                }
-
-                if(prev1 < nums1[i] && prev2 < nums2[i]){
-                    noSwap = 0 + dp[i+1][0];
-                }
-
-                dp[i][isSwap] = min(swap , noSwap);
-            }
-        }
-
-        return dp[0][0];
+        vector<vector<int>> dp(n , vector<int>(2, -1));
+        // return at i = 1 as we have changed the array ie. added -1 at 0th index
+        return minSwapHeler(nums1, nums2, 1, dp , 0);
     }
 };
