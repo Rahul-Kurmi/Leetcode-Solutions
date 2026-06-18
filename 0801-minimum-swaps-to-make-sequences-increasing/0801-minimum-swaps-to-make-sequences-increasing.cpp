e1ed630923470a47933826935a541c6f0236c9af
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-06-19 04:19:27 +0530

    Time: 84 ms (48.9%), Space: 136.6 MB (33.95%) - LeetHub

--- 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
+++ 0801-minimum-swaps-to-make-sequences-increasing/0801-minimum-swaps-to-make-sequences-increasing.cpp
@@ -1,23 +1,43 @@
 class Solution {
 public:
-    int minSwapHeler(vector<int>& nums1, vector<int>& nums2, int i, int prev1, int prev2){
+    int minSwapHeler(vector<int>& nums1, vector<int>& nums2, int i, vector<vector<int>>& dp, int isSwap){
         if(i == nums1.size()) return 0 ;
 
+        if(dp[i][isSwap] != -1) return dp[i][isSwap];
+
         int swap = INT_MAX , noSwap = INT_MAX ;
+
+        int prev1 , prev2 ; 
+        if(i == 0){ // this is neceasry as when i = 0, we can't acces (i-1)th index
+            prev1 = -1;
+            prev2 = -1;
+        }
+        else if(isSwap){
+            prev1 = nums2[i-1];
+            prev2 = nums1[i-1];
+        }
+        else{
+            prev1 = nums1[i-1];
+            prev2 = nums2[i-1];
+        }
+
+
         if(prev1 < nums2[i] && prev2 < nums1[i]){
             // now after swap --> new prev1 = nums2[i] , new prev2 = nums1[i]
-            swap = 1 + minSwapHeler(nums1, nums2, i+1, nums2[i], nums1[i]);
+            swap = 1 + minSwapHeler(nums1, nums2, i+1, dp, 1);
         }
 
         if(prev1 < nums1[i] && prev2 < nums2[i]){
             // swap not done as prev is already smaller --> this prev1 = nums1[i], prev2 = nums2[i]
-            noSwap = 0 + minSwapHeler(nums1, nums2, i+1, nums1[i] , nums2[i]);
+            noSwap = 0 + minSwapHeler(nums1, nums2, i+1, dp , 0);
         }
 
-        return min(swap , noSwap);
+        return dp[i][isSwap] = min(swap , noSwap);
     }
 
     int minSwap(vector<int>& nums1, vector<int>& nums2) {
-        return minSwapHeler(nums1, nums2, 0, -1 , -1);
+        int n = nums1.size();
+        vector<vector<int>> dp(n , vector<int>(2, -1));
+        return minSwapHeler(nums1, nums2, 0, dp , 0);
     }
 };
