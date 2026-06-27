c22ad7c468128436c4bc5a10e3672470a4cb2115
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-06-27 16:28:38 +0530

    Time: 23 ms (77.73%), Space: 31.4 MB (70.96%) - LeetHub

--- 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1312-minimum-insertion-steps-to-make-a-string-palindrome/1312-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -1,32 +1,27 @@
 class Solution {
 public:
+    // From the same bluprint of Longest palindromic substring 
     int minInsertions(string s) {
-        string t = s ;
-        reverse(t.begin() , t.end());
         int n = s.size();
-        vector<int> prev(n+1, 0);
-        vector<int> curr(n+1, 0);
+        vector<vector<int>> dp(n, vector<int> (n , 0));
 
-        // BASE CASE
-        for(int i = 0 ; i <= n ; i++) prev[i] = 0;
-        
-        for(int i = 1 ; i <= n ; i++){
-            for(int j = 1 ; j <= n ; j++){
-                int result = 0 ;
-                if(s[i - 1] == t[j - 1]){
-                    result = 1 + prev[j-1];
-                }
-                else{
-                    result = max(curr[j-1] , prev[j]);
-                }
-
-                curr[j] =  result ;
-            }
-            prev = curr ;
+        for(int i = 0 ; i < n ; i++){
+            dp[i][i] = 0 ; // single char is already pallindrome 
         }
 
 
-        int longPallSubsequence = prev[n];
-        return n - longPallSubsequence ;
+        for(int len = 2 ; len <= n ; len++){
+            for(int i = 0 ; len + i - 1 < n; i++){
+                int j = len + i - 1;
+                if(s[i] == s[j]){
+                    dp[i][j] = dp[i+1][j-1];
+                }
+                else{
+                    dp[i][j] = 1 + min(dp[i+1][j] , dp[i][j-1]);
+                }
+            }
+        }
+
+        return dp[0][n-1];
     }
 };
