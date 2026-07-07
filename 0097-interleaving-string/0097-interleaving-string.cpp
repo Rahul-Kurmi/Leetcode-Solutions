56e7e8ed66956766a1f60e877070459bdbadc607
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-07-08 02:18:28 +0530

    Time: 2 ms (65.4%), Space: 9.3 MB (55.87%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -30,6 +30,8 @@ public:
     }
 
     bool isInterleave(string s1, string s2, string s3) {
+        
+        // TABULATION CODE 
 
         if(s1.size() + s2.size() != s3.size())
             return false;
@@ -37,8 +39,43 @@ public:
         int m = s1.size();
         int n = s2.size();
 
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1, -1));
+        vector<vector<bool>> dp(m + 1, vector<bool>(n + 1, false));
 
-        return solve(s1, s2, s3, 0, 0, dp);
+        dp[m][n] = true ; // BC
+
+        for(int i = m; i >= 0; i--){
+            for(int j = n; j >= 0; j--){
+
+                if(i == m && j == n){ // already stored base case
+                    continue; 
+                }
+                // why start from i = m , j = n
+                // As we want to store ans when i = m and j = 0 --> n-1 
+                // also when j = n and i = 0 --> m-1
+
+                // Start from (m, n) because states on the last row (i = m)
+                // and last column (j = n) are also valid DP states.
+                // They represent the cases where one string is completely
+                // consumed while the other still has characters remaining.
+
+                int k = i + j;
+                bool result = false;
+
+                if(i < m && s1[i] == s3[k])
+                    result = dp[i+1][j];
+
+                if(result){
+                    dp[i][j] = true;
+                    continue;
+                }
+
+                if(j < n && s2[j] == s3[k])
+                    result = dp[i][j+1];
+
+                dp[i][j] = result;
+            }
+        }
+
+        return dp[0][0];
     }
 };
