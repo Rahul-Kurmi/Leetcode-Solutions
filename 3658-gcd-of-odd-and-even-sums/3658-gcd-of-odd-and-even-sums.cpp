1d54982cd09de5f32dd04d0696eac430fb66563f
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-07-15 22:44:39 +0530

    Time: 0 ms (100%), Space: 8.5 MB (85.02%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,11 +1,12 @@
-//Approach-1 (Simple Maths)
-//T.C : O(log(n))
+//Approach-2 (Simple Maths and Constant Time)
+//T.C : O(1)
 //S.C : O(1)
 class Solution {
 public:
     int gcdOfOddEvenSums(int n) {
         // Sum of first n odd numbers  = n * n
         // Sum of first n even numbers = n * (n + 1)
-        return __gcd(n * n, n * (n + 1)); 
+        // gcd(n*n, n*(n+1)) = n * gcd(n, n+1) = n * 1 = n
+        return n;
     }
 };
