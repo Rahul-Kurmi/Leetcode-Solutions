bc61b2981bcf2f75c1be858bcc3705a20ea82309
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-08-01 23:07:03 +0530

    Time: 0 ms (100%), Space: 10.1 MB (45.39%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -14,7 +14,21 @@ public:
 
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
-        vector<vector<int>> dp(n+1 , vector<int>(n+1, -1));
-        return solve(nums, 0 , n-1, dp) >= 0;
+        vector<vector<int>> dp(n+1 , vector<int>(n+1, 0));
+        // TABULATION
+        // BASE  CASE
+        for(int i = 0 ; i < n ; i++) dp[i][i] = nums[i]; 
+
+        for(int start = n-1 ; start >= 0 ; start--){
+            for(int end = start + 1  ; end < n ; end++){
+                // diff = P1 - P2
+                int diffByStart = nums[start] - dp[start+1][end];
+                int diffByEnd = nums[end] - dp[start][end-1];
+
+                dp[start][end] = max(diffByStart , diffByEnd);
+            }
+        }
+
+        return dp[0][n-1] >= 0;
     }
 };
