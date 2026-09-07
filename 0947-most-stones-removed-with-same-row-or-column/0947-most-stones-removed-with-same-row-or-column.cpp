4919218754fbeecc593dfd5ecfe4a6d8a980ec06
Rahul Kurmi <96816923+Rahul-Kurmi@users.noreply.github.com>
2026-09-07 10:07:20 +0530

    Time: 95 ms (5.9%), Space: 18.2 MB (92.51%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.cpp
@@ -1,55 +1,33 @@
-//T.C : O(n^2 * α(n))
-//S.C : O(n)
 class Solution {
 public:
-    vector<int> parent;
-    vector<int> rank;
-    int n;
-    
-    int find(int i) {
-        if(parent[i] != i)
-            parent[i] = find(parent[i]);
-        
-        return parent[i];
-    }
-    
-    void Union(int i, int j) {
-        int root_i = find(i);
-        int root_j = find(j);
-        
-        if(root_i != root_j) {
-            if(rank[root_i] > rank[root_j]) {
-                parent[root_j] = root_i;
-            } else  if(rank[root_i] < rank[root_j]) {
-                parent[root_i] = root_j;
-            } else {
-                parent[root_j] = root_i;
+    // DFS call, marking all the related stones visied and increasing count by 1 
+    // this helps us find no. of sets of related stones 
+
+    void dfs(int i, vector<vector<int>>& stones, vector<int>& visited){
+        visited[i] = 1 ;
+
+        for(int j = 0 ; j < stones.size(); j++){
+            if(!visited[j]){
+                // if any of row or col matches
+                if(stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]){
+                    dfs(j , stones, visited);
+                }
             }
         }
     }
-    
+
     int removeStones(vector<vector<int>>& stones) {
-        n = stones.size();
-        parent.resize(n);
-        rank.resize(n);
-        
-        for(int i = 0; i<n; i++) {
-            parent[i] = i;
-            rank[i] = 1;
-        }
-        
-        for(int i = 0; i<n; i++) {
-            for(int j = i+1; j<n; j++) {
-                if(stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1])
-                    Union(i, j);
+        int n = stones.size();
+        vector<int> visited(n , 0);
+
+        int count = 0 ;
+        for(int i = 0 ; i < n ; i++){
+            if(!visited[i]){
+                dfs(i , stones, visited);
+                count++;
             }
         }
-        
-        int groups = 0;
-        for(int i = 0; i<n; i++) {
-            if(parent[i] == i) groups++;
-        }
-        
-        return n-groups;
+
+        return n - count ;
     }
 };
